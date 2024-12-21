d0e65f8aac11b3204f33a2ab70ab1b04f84f65b9
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-21 23:52:30 +0530

    minor change in navbar

--- src/app/Components/commonComponent/NavBar.js
+++ src/app/Components/commonComponent/NavBar.js
@@ -5,13 +5,13 @@ import { TbNotification } from "react-icons/tb";
 function NavBar() {
   return (
     <div className=" flex justify-between p-2 items-center border-b-2 border-gray-300 md:border-0">
-        <nav className="flex md:ml-2 justify-around items-center">
+        <nav className="flex md:ml-2 ml-4 justify-around items-center">
         <Logo/>
         <SelectPage/>
         </nav>
         <div className="text-[20px] font-bold flex md:hidden">
             <FiBell className="mr-2"/>
-            <TbNotification className="mr-2"/>
+            <TbNotification className="mr-4"/>
         </div>
     </div>
   )
