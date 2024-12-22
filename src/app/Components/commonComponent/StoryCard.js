0df94415fe94a38aeed8d358d880c3a955c577c1
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-22 14:11:05 +0530

    cat image modify 8

--- src/app/Components/commonComponent/StoryCard.js
+++ src/app/Components/commonComponent/StoryCard.js
@@ -2,21 +2,21 @@ import React from 'react'
 import Cat from "../../../../public/cat.jpg"
 import Image from 'next/image'
 function StoryCard() {
-  return (
-    <>
-    <div className='border-2  relative border-gray-300  rounded-md mx-2 md:mx-4 h-[18vh] md:h-[24vh]'>
-        <div className='relative w-[100%] h-[100%]'>
-            <Image src={Cat} objectFit='cover' className='bg-red-200 h-[100%]' alt="cat" fill={true}/>
-        </div>
-        <span className='absolute top-[100%] text-center flex justify-center items-center left-1/2 transform -translate-x-1/2 -translate-y-1/2 aspect-auto rounded-full w-[1.9rem] h-[1.9rem] md:w-[1.7rem] md:h-[1.7rem] text-white text-[1rem]'>
-        <Image src={Cat} objectFit='cover' className='bg-red-200 h-[100%] rounded-full' alt="cat" fill={true}/>
-        </span>
+    return (
+        <>
+            <div className='border-2  relative border-gray-300  rounded-md mx-2 md:mx-4 h-[18vh] md:h-[24vh]'>
+                <div className='relative w-[100%] h-[100%]'>
+                    <Image src={Cat} objectFit='cover' className='bg-red-200 h-[100%]' alt="cat" fill={true} />
+                </div>
+                <span className='absolute top-[100%] text-center flex justify-center items-center left-1/2 transform -translate-x-1/2 -translate-y-1/2 aspect-auto rounded-full w-[1.9rem] h-[1.9rem] md:w-[1.7rem] md:h-[1.7rem] text-white text-[1rem]'>
+                    <Image src={Cat} objectFit='cover' className='bg-red-200 h-[100%] rounded-full' alt="cat" fill={true} />
+                </span>
 
-        
-    </div>
-    <h1 className='text-center mt-2 mb-2 font-medium'>elysa</h1>
-    </>
-  )
+
+            </div>
+            <h1 className='text-center mt-2 mb-2 font-medium'>elysa</h1>
+        </>
+    )
 }
 
 export { StoryCard }
