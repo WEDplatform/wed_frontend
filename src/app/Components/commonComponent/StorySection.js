936698d6d3b206511787a31c8aff2838edf9d83c
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-22 14:29:14 +0530

    cat image modify 10

--- src/app/Components/commonComponent/StorySection.js
+++ src/app/Components/commonComponent/StorySection.js
@@ -22,7 +22,7 @@ function StorySection() {
                     }
                 }}
                 hasTrack={false} aria-label="...">
-                <SplideTrack className="w-[100%] overflow-visible bg-red-300 md:w-[60vw] h-[18vh] md:h-[28vh] md:mt-4">
+                <SplideTrack className="w-[100%] overflow-visible md:w-[60vw] h-[18vh] md:h-[28vh] md:mt-4">
                     {
                         new Array(10).fill(0).map((_, pos) =>
                             <SplideSlide className="overflow-visible " key={pos}>
