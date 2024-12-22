964688f1329c1e38143cadf341d261908acb400a
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-22 13:41:51 +0530

    cat image modify

--- src/app/Components/commonComponent/StoryCard.js
+++ src/app/Components/commonComponent/StoryCard.js
@@ -7,7 +7,7 @@ function StoryCard() {
         <div className='relative w-[100%] h-[100%]'>
             <Image src={Cat} objectFit='cover' className='bg-red-200 h-[100%]' alt="cat" fill={true}/>
         </div>
-        <span className='absolute top-[100%] text-center flex justify-center items-center left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black z-10  rounded-full w-[1.7rem] h-[1.7rem] text-white text-[1rem]'>
+        <span className='absolute top-[100%] text-center flex justify-center items-center left-1/2 transform -translate-x-1/2 -translate-y-1/2    rounded-full w-[1.7rem] h-[1.7rem] text-white text-[1rem]'>
         <Image src={Cat} objectFit='cover' className='bg-red-200 h-[100%] rounded-full' alt="cat" fill={true}/>
         </span>
 
