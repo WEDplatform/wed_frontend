d2f40293aef187d1a9740cd0a75d7fc266e1afce
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-23 00:16:00 +0530

    initialize the infinit component phase 2

--- src/app/Components/commonComponent/Posts.js
+++ src/app/Components/commonComponent/Posts.js
@@ -3,7 +3,7 @@ import { getVideoUrl } from "@/app/apiFunctions/pexel"
 function Posts() {
   //getVideoUrl("Wedding")
   return (
-    <div className="md:w-[50vw] w-[100%]"></div>
+    <div className="md:w-[50vw] w-[100%] h-[100%] border-2"></div>
   )
 }
 
