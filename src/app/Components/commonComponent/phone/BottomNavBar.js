68ed609e47d174bb037b25bfe38f0ca3356dffe1
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-22 21:42:30 +0530

    added elemnt to navbar 1

--- src/app/Components/commonComponent/phone/BottomNavBar.js
+++ src/app/Components/commonComponent/phone/BottomNavBar.js
@@ -5,7 +5,7 @@ import { GoPerson } from "react-icons/go";
 function BottomNavBar() {
   return (
     <div className="w-[100%] md:hidden block border-t-[1px] border-gray-300 fixed bottom-0">
-        <div className="w-[100%] flex justify-between  h-[100%] bg-white py-4">
+        <div className="w-[100%] flex relative justify-between  h-[100%] bg-white py-4">
         <main className="w-[45%] flex justify-evenly ">
             <div className="flex flex-col items-center">
             <RiHome5Fill className="text-3xl" />
