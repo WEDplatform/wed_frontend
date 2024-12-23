8b3b6b586c5f66263d89b79fd69842a4bbb20103
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-23 21:55:25 +0530

    started for post structure 7

--- src/app/Components/commonComponent/PostStructure.js
+++ src/app/Components/commonComponent/PostStructure.js
@@ -1,27 +1,72 @@
 import Image from "next/image"
 import ICO from "@/app/favicon.ico"
+import { BsThreeDots } from "react-icons/bs";
+import { FaRegHeart } from "react-icons/fa";
+import { TbLocation } from "react-icons/tb";
+import { MdSaveAlt } from "react-icons/md";
 const ImagePost = () =>{
     return(
         <>
         
-        <div className="aspect-[4/3] border-2 border-gray-300 md:col-span-2 w-[100%] ">
-            <p className="flex items-center gap-2  py-1 pl-2">
-                <Image src={ICO} width={30} height={30}/>
-                <span>Username<span>Dubai</span></span>
-            </p>
+        <div className="aspect-[4/3] border-2 border-gray-300 md:col-span-3 w-[100%] ">
+            <div className="flex items-center justify-between gap-2  py-1 pl-2">
+                <main className="flex items-center gap-2">
+                <Image alt="pic" src={ICO} width={40} height={40}/>
+                <span className="flex flex-col">
+                    <span className="font-semibold">Username</span>
+                    <span className="font-normal text-gray-600">Dubai (UAE)</span>
+                </span>
+                </main>
+                <nav className="mr-3">
+                    <BsThreeDots />
+                </nav>
+            </div>
             <div className="w-[100%] h-[100%] rounded-xl bg-red-200">
                 Photo post
             </div>
-            <div className="bg-white">Likes</div>
+            <div className="bg-white flex justify-between w-[100%] py-2">
+                <div className="w-[30%] text-2xl justify-evenly flex items-center">
+                    <FaRegHeart/>
+                    <TbLocation/>
+                    <MdSaveAlt/>
+                </div>
+                <div className="w-[30%] flex justify-center items-center">
+                    <p className="text-[12px] mr-3 bg-[#FFECEC] px-3 font-semibold py-1 text-nowrap rounded-[25px]">2490 reviews</p>
+                </div>
+            </div>
         </div>
         </>
     )
 }
-const VideoPost =()=>{
+const VideoPost =()=> {
     return(
         <>
-        <div className="aspect-[9/16] md:col-span-1 w-[100%] bg-red-400">
-            Video post 
+         <div className="aspect-[9/16] border-2 border-gray-300 md:col-span-2 w-[100%] ">
+            <div className="flex items-center justify-between gap-2  py-1 pl-2">
+                <main className="flex items-center gap-2">
+                <Image alt="pic" src={ICO} width={40} height={40}/>
+                <span className="flex flex-col">
+                    <span className="font-semibold">Username</span>
+                    <span className="font-normal text-gray-600">Dubai (UAE)</span>
+                </span>
+                </main>
+                <nav className="mr-3">
+                    <BsThreeDots />
+                </nav>
+            </div>
+            <div className="w-[100%] h-[100%] rounded-xl bg-red-200">
+                Video post
+            </div>
+            <div className="bg-white flex justify-between w-[100%] py-2">
+                <div className="w-[40%] text-2xl justify-evenly flex items-center">
+                    <FaRegHeart/>
+                    <TbLocation/>
+                    <MdSaveAlt/>
+                </div>
+                <div className="w-[40%] flex justify-center items-center">
+                    <p className="text-[12px] mr-3 bg-[#FFECEC] px-3 font-semibold py-1 text-nowrap rounded-[25px]">2490 reviews</p>
+                </div>
+            </div>
         </div>
         </>
     )
--- src/app/Components/commonComponent/Posts.js
+++ src/app/Components/commonComponent/Posts.js
@@ -3,13 +3,18 @@ import { ImagePost,VideoPost } from "./PostStructure"
 function Posts() {
   //getVideoUrl("Wedding")
   return (
-    <div className="md:w-[50vw] preferenceList grid md:gap-2 grid-cols-1 overflow-y-auto gap-1 md:grid-cols-4 grid-flow-row w-[100%] h-[80%] border-2 md:ml-4">
+    <div className="md:w-[50vw] preferenceList grid md:gap-2 grid-cols-1 overflow-y-auto gap-1 auto-cols-fr md:grid-cols-6 w-[100%] h-[80%] border-2 md:ml-4">
         <ImagePost/>
         {/* <ImagePost/>
         <ImagePost/>
         <VideoPost/>
         <VideoPost/>
         <ImagePost/> */}
+        <VideoPost/>
+        <ImagePost/>
+        <ImagePost/>
+        <VideoPost/>
+        {/* <ImagePost/> */}
     </div>
   )
 }
