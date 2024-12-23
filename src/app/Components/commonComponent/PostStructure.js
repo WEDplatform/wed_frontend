a2fdcd25e2fb968492f11685e4a533acc0a738ce
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-23 22:51:16 +0530

    bg change in image post 8

--- src/app/Components/commonComponent/PostStructure.js
+++ src/app/Components/commonComponent/PostStructure.js
@@ -5,71 +5,71 @@ import { FaRegHeart } from "react-icons/fa";
 import { TbLocation } from "react-icons/tb";
 import { MdSaveAlt } from "react-icons/md";
 import { PostImageSlider } from "./PostImageSlider";
-const ImagePost = () =>{
-    return(
+const ImagePost = () => {
+    return (
         <>
-        
-        <div className="aspect-[4/3] row-span-2 border-2  rounded-xl md:col-span-4 w-[100%] ">
-            <div className="flex items-center justify-between gap-2  py-1 pl-2">
-                <main className="flex items-center gap-2">
-                <Image alt="pic" src={ICO} width={40} height={40}/>
-                <span className="flex flex-col">
-                    <span className="font-semibold">Username</span>
-                    <span className="font-normal text-gray-600">Dubai (UAE)</span>
-                </span>
-                </main>
-                <nav className="mr-3">
-                    <BsThreeDots />
-                </nav>
-            </div>
-            <div className="w-[100%] h-[100%] rounded-xl bg-gray-200">
-                <PostImageSlider/>
-            </div>
-            <div className="bg-white flex justify-between w-[100%] py-2">
-                <div className="w-[30%] text-2xl justify-evenly flex items-center">
-                    <FaRegHeart/>
-                    <TbLocation/>
-                    <MdSaveAlt/>
+
+            <div className="aspect-[4/3] row-span-2 border-2  rounded-xl md:col-span-4 w-[100%] ">
+                <div className="flex items-center justify-between gap-2  py-1 pl-2">
+                    <main className="flex items-center gap-2">
+                        <Image alt="pic" src={ICO} width={40} height={40} />
+                        <span className="flex flex-col">
+                            <span className="font-semibold">Username</span>
+                            <span className="font-normal text-gray-600">Dubai (UAE)</span>
+                        </span>
+                    </main>
+                    <nav className="mr-3">
+                        <BsThreeDots />
+                    </nav>
                 </div>
-                <div className="w-[30%] flex justify-center items-center">
-                    <p className="text-[12px] mr-3 bg-[#FFECEC] px-3 font-semibold py-1 text-nowrap rounded-[25px]">2490 reviews</p>
+                <div className="w-[100%] h-[100%] rounded-xl bg-gray-200">
+                    <PostImageSlider />
+                </div>
+                <div className="bg-white flex justify-between w-[100%] py-2">
+                    <div className="w-[30%] text-2xl justify-evenly flex items-center">
+                        <FaRegHeart />
+                        <TbLocation />
+                        <MdSaveAlt />
+                    </div>
+                    <div className="w-[30%] flex justify-center items-center">
+                        <p className="text-[12px] mr-3 bg-[#FFECEC] px-3 font-semibold py-1 text-nowrap rounded-[25px]">2490 reviews</p>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
-const VideoPost =()=> {
-    return(
+const VideoPost = () => {
+    return (
         <>
-         <div className="aspect-[9/16] row-span-3 border-2 rounded-xl md:col-span-2 w-[100%] ">
-            <div className="flex items-center justify-between gap-2  py-1 pl-2">
-                <main className="flex items-center gap-2">
-                <Image alt="pic" src={ICO} width={40} height={40}/>
-                <span className="flex flex-col">
-                    <span className="font-semibold">Username</span>
-                    <span className="font-normal text-gray-600">Dubai (UAE)</span>
-                </span>
-                </main>
-                <nav className="mr-3">
-                    <BsThreeDots />
-                </nav>
-            </div>
-            <div className="w-[100%] h-[100%] rounded-xl bg-gray-200">
-                Video post
-            </div>
-            <div className="bg-white flex justify-between w-[100%] py-2">
-                <div className="w-[40%] text-2xl justify-evenly flex items-center">
-                    <FaRegHeart/>
-                    <TbLocation/>
-                    <MdSaveAlt/>
+            <div className="aspect-[9/16] row-span-3 border-2 rounded-xl md:col-span-2 w-[100%] ">
+                <div className="flex items-center justify-between gap-2  py-1 pl-2">
+                    <main className="flex items-center gap-2">
+                        <Image alt="pic" src={ICO} width={40} height={40} />
+                        <span className="flex flex-col">
+                            <span className="font-semibold">Username</span>
+                            <span className="font-normal text-gray-600">Dubai (UAE)</span>
+                        </span>
+                    </main>
+                    <nav className="mr-3">
+                        <BsThreeDots />
+                    </nav>
                 </div>
-                <div className="w-[40%] flex justify-center items-center">
-                    <p className="text-[12px] mr-3 bg-[#FFECEC] px-3 font-semibold py-1 text-nowrap rounded-[25px]">2490 reviews</p>
+                <div className="w-[100%] h-[100%] rounded-xl bg-gray-200">
+                    Video post
+                </div>
+                <div className="bg-white flex justify-between w-[100%] py-2">
+                    <div className="w-[40%] text-2xl justify-evenly flex items-center">
+                        <FaRegHeart />
+                        <TbLocation />
+                        <MdSaveAlt />
+                    </div>
+                    <div className="w-[40%] flex justify-center items-center">
+                        <p className="text-[12px] mr-3 bg-[#FFECEC] px-3 font-semibold py-1 text-nowrap rounded-[25px]">2490 reviews</p>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
-export {ImagePost,VideoPost}
+export { ImagePost, VideoPost }
