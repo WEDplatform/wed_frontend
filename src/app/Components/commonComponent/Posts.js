0dd47bfbca30b64951cc20f64658e7eb69b9b41a
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-23 22:23:43 +0530

    bg change in image post 3

--- src/app/Components/commonComponent/Posts.js
+++ src/app/Components/commonComponent/Posts.js
@@ -3,7 +3,7 @@ import { ImagePost,VideoPost } from "./PostStructure"
 function Posts() {
   //getVideoUrl("Wedding")
   return (
-    <div className="md:w-[70vw] preferenceList grid md:gap-2 grid-cols-1 overflow-y-auto gap-1 auto-rows-min md:grid-cols-8 w-[100%] h-[70%] md:h-[80%] border-2 md:ml-4">
+    <div className="md:w-[70vw] pt-2 preferenceList grid md:gap-2 grid-cols-1 overflow-y-auto gap-1 auto-rows-min md:grid-cols-8 w-[100%] h-[70%] md:h-[80%] md:ml-4">
         <ImagePost/>
         {/* <ImagePost/>
         <ImagePost/>
