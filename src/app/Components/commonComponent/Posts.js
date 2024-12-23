66574293006605d0eec41265b975119d3af2081f
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-23 14:19:37 +0530

    started for post structure 3

--- src/app/Components/commonComponent/Posts.js
+++ src/app/Components/commonComponent/Posts.js
@@ -6,6 +6,8 @@ function Posts() {
     <div className="md:w-[50vw] preferenceList grid md:gap-2 grid-cols-1 overflow-y-auto md:grid-cols-2 grid-flow-row w-[100%] h-[90%] border-2 md:ml-4">
         <ImagePost/>
         <ImagePost/>
+        <ImagePost/>
+
     </div>
   )
 }
