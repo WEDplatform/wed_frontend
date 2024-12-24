bfb99ad6a40643aa993fcafb6575ce38163593b1
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-24 09:25:10 +0530

    changed image post slider

--- src/app/Components/commonComponent/Posts.js
+++ src/app/Components/commonComponent/Posts.js
@@ -1,9 +1,12 @@
 import { getVideoUrl } from "@/app/apiFunctions/pexel"
 import { ImagePost,VideoPost } from "./PostStructure"
+import { StorySection } from "./StorySection"
 function Posts() {
   //getVideoUrl("Wedding")
   return (
-    <div className="md:w-[70vw] pt-2 preferenceList grid md:gap-2 grid-cols-1 overflow-y-auto gap-1 auto-rows-min md:grid-cols-8 w-[100%] h-[70%] md:h-[80%] md:ml-4">
+    <>
+      <div className="md:w-[70vw] pt-2 preferenceList grid md:gap-2 grid-cols-1 overflow-y-auto gap-1 auto-rows-min md:grid-cols-8 w-[100%] h-[70%] md:h-[80%] md:ml-4">
+        
         <ImagePost/>
         {/* <ImagePost/>
         <ImagePost/>
@@ -19,6 +22,7 @@ function Posts() {
         <VideoPost/>
         <ImagePost/>
     </div>
+    </>
   )
 }
 
