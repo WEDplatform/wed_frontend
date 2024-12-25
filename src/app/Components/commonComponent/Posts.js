8e3f53c549b0ed7503a9ba4f41f9dac901ad3ccb
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-25 13:47:26 +0530

    story carousel bg remove 2

--- src/app/Components/commonComponent/Posts.js
+++ src/app/Components/commonComponent/Posts.js
@@ -1,21 +1,34 @@
 import { getVideoUrl } from "@/app/apiFunctions/pexel"
-import { ImagePost,VideoPost } from "./PostStructure"
+import { ImagePost, VideoPost } from "./PostStructure"
 import { StorySection } from "./StorySection"
 import { getImageUrl } from "@/app/apiFunctions/pexel"
-function Posts() { 
-  let rnd=Math.floor((Math.random() * 10) + 1)
+function Posts() {
+  let rnd = Math.floor((Math.random() * 10) + 1)
   console.log(rnd);
   //getVideoUrl("Wedding")
   //getImageUrl("Wedding")
   //getVideoUrl("Wedding")
   return (
     <>
-      <div className="md:w-[95%] pt-2 preferenceList grid md:gap-2 grid-cols-1 md:overflow-y-auto gap-1 auto-rows-min md:grid-cols-6 w-[100%] h-[90%] md:h-[80%] md:ml-4">
-        
+      <div className="md:w-[95%] md:pt-2 hidden md:block  border-2 w-[100%] h-[90%] md:h-[73vh] md:ml-4">
+        <div className="w-[100%] h-[100%] flex">
+          <main className="w-[65%] preferenceList h-[100%] border-2 overflow-y-auto">
+            <ImagePost />
+            <ImagePost />
+          </main>
+          <main className="w-[35%] preferenceList h-[100%] border-2 overflow-y-auto">
+            <VideoPost />
+          </main>
+        </div>
+
+
+
+      </div>
+      <div className="w-[100%] md:hidden mt-2">
         {
-          new Array(5).fill(0).map((_,pos)=>pos%rnd===0?<ImagePost key={pos} pageIndex={pos}/>:<VideoPost key={pos} pageIndex={pos}/>)
+          new Array(5).fill(0).map((_, pos) => pos % rnd === 0 ? <ImagePost key={pos} pageIndex={pos} /> : <VideoPost key={pos} pageIndex={pos} />)
         }
-    </div>
+      </div>
     </>
   )
 }
