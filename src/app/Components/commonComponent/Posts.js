6a39bb83fe848795d5d4d9b47c7084983efa8447
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-25 11:09:37 +0530

    init of basic changes at post section

--- src/app/Components/commonComponent/Posts.js
+++ src/app/Components/commonComponent/Posts.js
@@ -10,7 +10,7 @@ function Posts() {
   //getVideoUrl("Wedding")
   return (
     <>
-      <div className="md:w-[50vw] pt-2 preferenceList grid md:gap-2 grid-cols-1 overflow-y-auto gap-1 auto-rows-min md:grid-cols-6 w-[100%] h-[70%] md:h-[80%] md:ml-4">
+      <div className="md:w-[97%] pt-2 preferenceList grid md:gap-2 grid-cols-1 overflow-y-auto gap-1 auto-rows-min md:grid-cols-6 w-[100%] h-[70%] md:h-[80%] md:ml-4">
         
         {
           new Array(5).fill(0).map((_,pos)=>pos%rnd===0?<ImagePost key={pos} pageIndex={pos}/>:<VideoPost key={pos} pageIndex={pos}/>)
--- src/app/home/[clientType]/page.js
+++ src/app/home/[clientType]/page.js
@@ -17,7 +17,7 @@ function page() {
           <NavBar />
           <SideBarMain />
         </div>
-        <div className='md:w-[80%] flex flex-col items-center relative md:items-start h-[100%] w-[100vw]'>
+        <div className='md:w-[55%] bg-red-200 flex flex-col items-center relative md:items-start h-[100%] w-[100vw]'>
           <StorySection />
           <Posts />
         </div>
