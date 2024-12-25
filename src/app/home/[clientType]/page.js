52bbc6d3c931becfbc5cc19cbbc79f8b3d2f38f2
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-25 12:06:28 +0530

    page home modification

--- src/app/home/[clientType]/page.js
+++ src/app/home/[clientType]/page.js
@@ -9,18 +9,20 @@ import { Posts } from "@/app/Components/commonComponent/Posts"
 function page() {
   return (
     <>
-      <div className='flex md:flex-row h-[100vh] flex-col'>
-        <div className="md:hidden w-[100vw] ">
-          <NavBar />
-        </div>
+      <div className='flex md:flex-row h-[90vh] md:h-[100vh] overflow-y-auto flex-col'>
+        
         <div className='w-[20%] hidden md:block border-r-2 border-gray-300 h-screen'>
           <NavBar />
           <SideBarMain />
         </div>
-        <div className='md:w-[55%] flex flex-col items-center relative md:items-start h-[100%] w-[100vw]'>
-          <div className="w-[100%] bg-red-200 hidden md:block">
+        <div className="md:hidden w-[100vw] ">
+          <NavBar />
+        </div>
+        <div className="w-[100%] bg-red-200 block md:hidden">
             <div className="w-[100%]  flex justify-center"><StorySection /></div>
           </div>
+        <div className='md:w-[55%] flex flex-col items-center relative md:items-start h-[100%] w-[100vw]'>
+          
           <Posts />
         </div>
       </div>
