b570e8ee7e71785772719550ef35a4a8bf3cb0c5
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-26 17:19:45 +0530

    log removded 1

--- src/app/Components/commonComponent/Posts.js
+++ src/app/Components/commonComponent/Posts.js
@@ -7,7 +7,6 @@ function Posts() {
   //console.log(rnd);
   //getVideoUrl("Wedding")
   //getImageUrl("Wedding")
-  //getVideoUrl("Wedding")
   return (
     <>
       <div className="md:w-[95%] md:pt-2 hidden md:block  w-[100%] h-[90%] md:h-[73vh] md:ml-4">
