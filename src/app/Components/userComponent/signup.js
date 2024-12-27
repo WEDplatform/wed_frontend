0e7353c61c92bd74bc6d537227dca85eedbdc846
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-27 23:16:57 +0530

    basic post changesin swr 3

--- src/app/Components/userComponent/signup.js
+++ src/app/Components/userComponent/signup.js
@@ -54,8 +54,8 @@ const [pageTracker,updateTracker]=useState({
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite:false,
-    nextArrow:<p></p>, //only empty string
-    prevArrow:<p></p>
+    nextArrow:"", //only empty string
+    prevArrow:""
   };
   let sliderRef = useRef(null);
   const next = () => {
