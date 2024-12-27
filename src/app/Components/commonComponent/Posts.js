336987db588f305046e369caf742e5da5db86526
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-27 09:49:30 +0530

    uninstalled react infinity scroll

--- src/app/Components/commonComponent/Posts.js
+++ src/app/Components/commonComponent/Posts.js
@@ -2,19 +2,18 @@ import { getVideoUrl } from "@/app/apiFunctions/pexel"
 import { ImagePost, VideoPost } from "./PostStructure"
 import { StorySection } from "./StorySection"
 import { getImageUrl } from "@/app/apiFunctions/pexel"
-import InfiniteScroll from "react-infinite-scroll-component"
 function Posts() {
   let rnd = Math.floor((Math.random() * 10) + 1)
   return (
     <>
       <div className="md:w-[95%] md:pt-2 hidden md:block  w-[100%] h-[90%] md:h-[73vh] md:ml-4">
         <div className="w-[100%] h-[100%] flex justify-between">
-          <main className="w-[64%] preferenceList h-[100%] overflow-y-auto">
-            {
+          <main id="ImagePost" className="w-[64%] preferenceList h-[100%] overflow-y-auto">
+          {
               new Array(2).fill(0).map((_, pos) =><ImagePost key={pos} pageIndex={pos+1} />)
             }
           </main>
-          <main className="w-[35%] preferenceList h-[100%] overflow-y-auto">
+          <main id="VideoPost" className="w-[35%] preferenceList h-[100%] overflow-y-auto">
             <VideoPost />
             <VideoPost />
 
@@ -24,7 +23,7 @@ function Posts() {
 
 
       </div>
-      <div className="w-[100%] md:hidden mt-2">
+      <div id="MobilePost" className="w-[100%] md:hidden mt-2">
         {
           new Array(5).fill(0).map((_, pos) => pos % rnd === 0 ? <ImagePost key={pos} pageIndex={pos+1} /> : <VideoPost key={pos} pageIndex={pos} />)
         }
