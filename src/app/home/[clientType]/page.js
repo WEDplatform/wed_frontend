a1b4a1f6f0d10f988408892104f0c6f560ebbc5a
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-29 02:23:57 +0530

    right side modify 1

--- src/app/home/[clientType]/page.js
+++ src/app/home/[clientType]/page.js
@@ -32,7 +32,7 @@ async function page({params,searchParams}) {
           <Posts id_={"mainPost"}/>
         </div>
         <div className="hidden md:block w-[25%] h-[100%] border-2">
-            <div className="w-[100%] h-[100%] bg-red-200">
+            <div className="w-[100%] h-[100%]">
             {
               searchprm.tab
             }
