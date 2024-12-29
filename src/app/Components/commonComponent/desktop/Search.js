21ce51b2f758f675b4c7deb5ea28ccdfe7407a18
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-30 00:06:20 +0530

    added function for query search

--- src/app/Components/commonComponent/desktop/Search.js
+++ src/app/Components/commonComponent/desktop/Search.js
@@ -5,18 +5,41 @@ import { searchSection } from "@/app/lib/constants";
 import { useState } from "react";
 function Search() {
   const [searchQuery,setQuery]=useState("");
-  
+  const [filterList,setFiterList]=useState([]);
+  const filterQueries = (query) =>{
+    setQuery(query)
+    if(query=="") {
+      setFiterList(searchSection)
+      return
+    }
+    let filteredQuery = searchSection
+  .map((item) => ({
+    title: item.title,
+    value: item.value.filter((items) =>
+      items.toLowerCase().includes(query.trim().toLowerCase())
+    ),
+  }))
+  .filter((item) => item.value.length > 0); // Remove items with no matching values
+
+console.log(filteredQuery);
+
+    if(filteredQuery.length==0) return
+    setFiterList(filteredQuery)
+    
+  }
   return (
     <div className="w-[90%] h-[95%]">
         <p className="text-sm text-gray-500">Search</p>
         <div className="relative">
         <RiSearch2Line className="absolute text-gray-500 top-2 left-3 text-[25px]"/>    
-        <input value={searchQuery} onChange={(e)=>{setQuery(e.target.value.toLowerCase())}} placeholder="Search keyword" type="text" className="w-[100%] text-sm text-gray-600 px-4 py-2 pl-10 bg-gray-200 rounded-lg outline-none  border-2 border-gray-300"/></div>
+        <input value={searchQuery} onChange={(e)=>{filterQueries(e.target.value.toLowerCase())}} placeholder="Search keyword" type="text" className="w-[100%] text-sm text-gray-600 px-4 py-2 pl-10 bg-gray-200 rounded-lg outline-none  border-2 border-gray-300"/></div>
         <div className="w-[100%] h-[90%] overflow-y-auto">
             {
-                searchSection.map((item,pos)=>
-                    <SearchSectionScroll key={pos} title={item.title} vals={item.value}/>
-                )
+                searchQuery != "" ? filterList.map((item,pos)=>
+                  <SearchSectionScroll key={pos} title={item.title} vals={item.value}/>
+              ) : searchSection.map((item,pos)=>
+                <SearchSectionScroll key={pos} title={item.title} vals={item.value}/>
+            )
             }
         </div>
     </div>
