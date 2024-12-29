f7a86cfc88de96f9dacbf61b751cd72186d93a08
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-29 17:37:51 +0530

    search part 2

--- src/app/Components/commonComponent/desktop/Search.js
+++ src/app/Components/commonComponent/desktop/Search.js
@@ -1,6 +1,12 @@
+import { RiSearch2Line } from "react-icons/ri";
 function Search() {
   return (
-    <div className="w-[90%] h-[95%]">Search</div>
+    <div className="w-[90%] h-[95%]">
+        <p className="text-sm text-gray-500">Search</p>
+        <div className="relative">
+        <RiSearch2Line className="absolute text-gray-500 top-2 left-3 text-[25px]"/>    
+        <input placeholder="Search keyword" type="text" className="w-[100%] text-sm text-gray-600 px-4 py-2 pl-10 bg-gray-200 rounded-lg outline-none  border-2 border-gray-300"/></div>
+    </div>
   )
 }
 
