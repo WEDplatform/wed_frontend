d69de3f4859316f9433da11690ce47fa7b3de32e
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-30 01:26:46 +0530

    profile code in page 3

--- src/app/Components/commonComponent/Profile.js
+++ src/app/Components/commonComponent/Profile.js
@@ -1,8 +1,13 @@
+import Image from "next/image"
+import profile from "../../../../public/profile.svg"
+import { MdCameraswitch } from "react-icons/md";
 function Profile() {
   return (
     <div className="w-[90%] md:overflow-y-auto h-[100vh]  md:h-[95%] flex flex-col items-center">
-        <div className="w-[10vw] h-[10vw] bg-gray-200 rounded-full overflow-hidden">
-            
+        <div className="w-[10vw] h-[10vw] bg-gray-200 relative rounded-full">
+            <input type="file" className="hidden"/>
+            <Image alt="profilePic" style={{objectFit:"cover"}} src={profile} />
+            <MdCameraswitch className="absolute top-[75%] bg-white w-[1.6vw] rounded-full border-2 border-gray-600 h-[1.6vw] p-1 right-[5%]"/>
         </div>
     </div>
   )
