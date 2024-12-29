30c95e2be7366dbd9c2f189668965d9cf2e8409e
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-29 19:05:57 +0530

    search section scroll part 5

--- src/app/Components/commonComponent/desktop/SearchSectionScroll.js
+++ src/app/Components/commonComponent/desktop/SearchSectionScroll.js
@@ -2,7 +2,7 @@ function SearchSectionScroll() {
   return (
     <div className="w-[100%] ">
         <p>Title</p>
-        <div className="w-[15vw] h-[10vh] flex bg-red-200 overflow-auto">
+        <div className="w-[15vw] h-[10vh] grid grid-rows-1 grid-flow-col bg-red-200 overflow-x-scroll">
             {
                 new Array(10).fill(0).map((item,pos)=>
                 <div key={pos} className="w-[10vw] ml-2 h-[3vh] bg-gray-200">
