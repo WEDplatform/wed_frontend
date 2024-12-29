cd4f0e5502542ce935a90d7549240beedd0c8a14
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-29 18:49:44 +0530

    search section scroll part 4

--- src/app/Components/commonComponent/desktop/SearchSectionScroll.js
+++ src/app/Components/commonComponent/desktop/SearchSectionScroll.js
@@ -1,16 +1,16 @@
 function SearchSectionScroll() {
   return (
-    <div className="w-[100%]">
+    <div className="w-[100%] ">
         <p>Title</p>
-        <main className="flex flex-nowrap overflow-x-auto">
+        <div className="w-[15vw] h-[10vh] flex bg-red-200 overflow-auto">
             {
-                new Array(5).fill(0).map((item,pos)=>
-                <div key={pos} className="w-[2vw] h-[3vh] bg-gray-200">
-
+                new Array(10).fill(0).map((item,pos)=>
+                <div key={pos} className="w-[10vw] ml-2 h-[3vh] bg-gray-200">
+h
                 </div>
                 )
             }
-        </main>
+        </div>
     </div>
   )
 }
