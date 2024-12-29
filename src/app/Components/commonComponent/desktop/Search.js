b47f2a6159c1c37c8feda400f9786a9e302a9f78
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-29 17:53:41 +0530

    search part 3

--- src/app/Components/commonComponent/desktop/Search.js
+++ src/app/Components/commonComponent/desktop/Search.js
@@ -6,6 +6,7 @@ function Search() {
         <div className="relative">
         <RiSearch2Line className="absolute text-gray-500 top-2 left-3 text-[25px]"/>    
         <input placeholder="Search keyword" type="text" className="w-[100%] text-sm text-gray-600 px-4 py-2 pl-10 bg-gray-200 rounded-lg outline-none  border-2 border-gray-300"/></div>
+        
     </div>
   )
 }
