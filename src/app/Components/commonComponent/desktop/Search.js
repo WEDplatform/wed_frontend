6fb9a04ee2f3cbcc916074fd7adca9fe8e45b7dc
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-29 18:35:28 +0530

    search section scroll part 3

--- src/app/Components/commonComponent/desktop/Search.js
+++ src/app/Components/commonComponent/desktop/Search.js
@@ -1,4 +1,5 @@
 import { RiSearch2Line } from "react-icons/ri";
+import { SearchSectionScroll } from "./SearchSectionScroll";
 function Search() {
   return (
     <div className="w-[90%] h-[95%]">
@@ -6,7 +7,7 @@ function Search() {
         <div className="relative">
         <RiSearch2Line className="absolute text-gray-500 top-2 left-3 text-[25px]"/>    
         <input placeholder="Search keyword" type="text" className="w-[100%] text-sm text-gray-600 px-4 py-2 pl-10 bg-gray-200 rounded-lg outline-none  border-2 border-gray-300"/></div>
-        
+        <SearchSectionScroll/>
     </div>
   )
 }
--- src/app/Components/commonComponent/desktop/SearchSectionScroll.js
+++ src/app/Components/commonComponent/desktop/SearchSectionScroll.js
@@ -2,8 +2,14 @@ function SearchSectionScroll() {
   return (
     <div className="w-[100%]">
         <p>Title</p>
-        <main>
-            
+        <main className="flex flex-nowrap overflow-x-auto">
+            {
+                new Array(5).fill(0).map((item,pos)=>
+                <div key={pos} className="w-[2vw] h-[3vh] bg-gray-200">
+
+                </div>
+                )
+            }
         </main>
     </div>
   )
