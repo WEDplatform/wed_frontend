f67b6982ed558bd310a00c60091dbee0868be0f6
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-29 22:09:29 +0530

    search section scroll part 6

--- src/app/Components/commonComponent/desktop/SearchSectionScroll.js
+++ src/app/Components/commonComponent/desktop/SearchSectionScroll.js
@@ -1,15 +1,34 @@
+'use client'
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 function SearchSectionScroll() {
   return (
     <div className="w-[100%] ">
         <p>Title</p>
-        <div className="w-[15vw] h-[10vh] grid grid-rows-1 grid-flow-col bg-red-200 overflow-x-scroll">
-            {
+        <Splide
+        options={{
+            perPage:3
+        }}
+        className="w-[100%]  flex justify-center h-[15vh]" aria-label="" hasTrack={false}>
+                    <SplideTrack className="w-[92%] overflow-hidden h-100%]  rounded-xl">
+                    {
                 new Array(10).fill(0).map((item,pos)=>
-                <div key={pos} className="w-[10vw] ml-2 h-[3vh] bg-gray-200">
-h
-                </div>
+                <SplideSlide key={pos} className="flex justify-center">
+                    <div className='w-[95%] h-[15vh] bg-white border-2'>
+                        h
+                    </div>
+                </SplideSlide>
                 )
             }
+                    </SplideTrack>
+                    <div className="splide__arrows  md:flex justify-between w-[100%] px-0 absolute top-[50%] transform -translate-y-1/2">
+                        <button className="splide__arrow  md:block splide__arrow--prev bg-white aspect-square rounded-full border-2 p-2 w-[2vw] h-[2vw]"><FaArrowLeft className="w-[100%] h-[100%] text-xl" /></button>
+                        <button className="splide__arrow  md:block splide__arrow--next bg-white aspect-square rounded-full border-2 p-2 w-[2vw] h-[2vw]"><FaArrowRight className="w-[100%] h-[100%] text-xl" /></button>
+                    </div>
+                </Splide>
+        <div >
+            
         </div>
     </div>
   )
