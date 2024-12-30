595a8774db8ae95c703a262c86b2eeaa7284b9f1
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-30 20:11:51 +0530

    profile api init 3

--- src/app/apiFunctions/profile.js
+++ src/app/apiFunctions/profile.js
@@ -1,4 +1,8 @@
+import { cookies } from 'next/headers'
+
 export const profie = async () =>{
+    const cookieStore = await cookies()  
+    
     try {
         await fetch(`${process.env.backend_api}/cmd/profile`,{})
     } catch (error) {
