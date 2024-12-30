52a678d234b4e989b545517f090df42cd345a28a
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-30 20:18:32 +0530

    profile api init 4

--- src/app/apiFunctions/profile.js
+++ src/app/apiFunctions/profile.js
@@ -2,10 +2,18 @@ import { cookies } from 'next/headers'
 
 export const profie = async () =>{
     const cookieStore = await cookies()  
-    
+
     try {
-        await fetch(`${process.env.backend_api}/cmd/profile`,{})
+        let profileResponse=await fetch(`${process.env.backend_api}/cmd/profile`,{
+            method:'GET',
+            headers:{
+                "wedoraCredentials":cookieStore?.get("refreshToken")?.value
+            }
+        })
+        profileResponse=await profileResponse.json()
+        
+        return profileResponse
     } catch (error) {
         
     }
-}
+}  
