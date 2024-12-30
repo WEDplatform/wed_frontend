5aeae91c70420bda612f8160b029eb2f570777eb
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-31 00:27:10 +0530

    basic profile section 1

--- src/app/Components/commonComponent/phone/BottomNavBar.js
+++ src/app/Components/commonComponent/phone/BottomNavBar.js
@@ -8,7 +8,7 @@ function BottomNavBar() {
     <div className="w-[100%] md:hidden block border-t-[1px] border-gray-300 fixed bottom-0">
         <div className="w-[100%] flex relative justify-between  h-[100%] bg-white py-4">
         <main className="w-[45%] flex justify-evenly ">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col text-[#9A2143] items-center">
             <RiHome5Fill className="text-3xl" />
             <span className="text-[0.8rem]">Home</span>
             </div>
