cf9c76ad7618a1b67bb6951b9c8661170dec73f0
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-31 22:55:27 +0530

    profile paage user part 1

--- src/app/profile/[clientType]/page.js
+++ src/app/profile/[clientType]/page.js
@@ -8,8 +8,11 @@ async function page({params}) {
             Profile
         </div>
         <div className="w-[79%] ">
-            <div className="h-[30vh] bg-red-200">
-
+            <div className="h-[30vh] grid grid-cols-2 grid-rows-3 bg-red-200">
+            <div className="bg-red-300 flex flex-col justify-center pl-3">
+                <p>Username</p>
+                <h1>Ujjwal</h1>
+            </div>
             </div>
             <div className="min-h-[30vh] bg-gray-300 mt-4">
 
