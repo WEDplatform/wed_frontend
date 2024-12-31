565f8cab67e1f330a8f8677a385ed7d5fe4bc4b0
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2024-12-31 17:33:22 +0530

    profile page 10

--- src/app/profile/[clientType]/page.js
+++ src/app/profile/[clientType]/page.js
@@ -3,9 +3,17 @@ async function page({params}) {
     console.log(clientType);
     
   return (
-    <div className="w-[80%] bg-red-200 mx-auto">
-        <div>
+    <div className="w-[80%] mt-5 flex justify-between mx-auto">
+        <div className="w-[20%] aspect-square bg-white">
             Profile
+        </div>
+        <div className="w-[79%] ">
+            <div className="h-[30vh] bg-red-200">
+
+            </div>
+            <div className="min-h-[30vh] bg-gray-300 mt-4">
+
+            </div>
         </div>
     </div>
   )
