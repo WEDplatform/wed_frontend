4d1c37ed1322b95766cdf7dd12d282e9380aede5
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-01 12:06:10 +0530

    shadow in div

--- src/app/profile/[clientType]/page.js
+++ src/app/profile/[clientType]/page.js
@@ -1,3 +1,4 @@
+import profilePic from "../../../../public/profile.svg"
 async function page({params}) {
     let {clientType}= await params
     console.log(clientType);
@@ -8,11 +9,12 @@ async function page({params}) {
             Profile
         </div>
         <div className="w-[79%] ">
-            <div className="h-[30vh] grid grid-cols-2 grid-rows-3">
+            <div className="h-[30vh] grid rounded-md grid-cols-2 grid-rows-3 shadow-xl shadow-gray-200">
             {/* <div className="bg-red-300 flex flex-col justify-center pl-3">
                 <p>Username</p>
                 <h1>Ujjwal</h1>
             </div> */}
+            <p className="mt-2 ml-2 text-sm font-semibold text-gray-500">Your analytics</p>
             </div>
             <div className="min-h-[30vh] bg-gray-300 mt-4">
 
