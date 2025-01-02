072684e553495fe94d842bc82ecd76075a86c021
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-02 10:39:08 +0530

    video post bit modification phase 2

--- src/app/Components/commonComponent/PostStructure.js
+++ src/app/Components/commonComponent/PostStructure.js
@@ -44,7 +44,7 @@ const ImagePost =  ({ images }) => {
         </>
     )
 }
-const VideoPost = async({pageIndex}) => {
+const VideoPost = async({pageIndex,vid}) => {
     // const router = useRouter()
     const videoResponse=await getVideoUrl("Wedding",pageIndex)
     return (
@@ -52,7 +52,7 @@ const VideoPost = async({pageIndex}) => {
             <div className="md:aspect-[3/4] my-1 aspect-[3/4] row-span-3 border-2 rounded-xl md:col-span-2 w-[100%] ">
                 <div className="flex items-center justify-between gap-2  py-1 pl-2">
                     
-                    <VendorDetails/>
+                    <VendorDetails vid={vid}/>
 
 
                     <nav className="mr-3 ml-2">
