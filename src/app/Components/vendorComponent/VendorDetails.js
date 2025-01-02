3b590224929720d68b521ece29fcd8d60f429993
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-02 10:28:55 +0530

    vendor details part init 2

--- src/app/Components/vendorComponent/VendorDetails.js
+++ src/app/Components/vendorComponent/VendorDetails.js
@@ -1,11 +1,19 @@
 'use client'
 import React from 'react'
-
+import Image from 'next/image'
+import ICO from "@/app/favicon.ico"
 function VendorDetails() {
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <main className="flex items-center cursor-pointer gap-2">
+                <Image alt="pic" src={ICO} width={40} height={40} />
+                <span className="flex flex-col">
+                    <span className="font-semibold">Username</span>
+                    <span className="font-normal text-[12px] text-gray-600">Dubai (UAE)</span>
+                </span>
+            </main>
+        </>
+    )
 }
 
 export default VendorDetails
