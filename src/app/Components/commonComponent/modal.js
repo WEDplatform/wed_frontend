4946bf8b74f53460fb157c939849761849bfcad8
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-09 18:11:03 +0530

    b change 4

--- src/app/Components/commonComponent/modal.js
+++ src/app/Components/commonComponent/modal.js
@@ -5,7 +5,6 @@ function Modal({ children }) {
         <div className="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex mt-10 items-end justify-center p-4 text-center sm:items-center sm:p-0">
-
             <div className="relative transform overflow-hidden rounded-lg  text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
               <div className="bg-white md:px-4 md:pb-4 p-4 sm:p-6 ">
                 {children}
