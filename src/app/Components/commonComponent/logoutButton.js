b915f17234feb5c20e402510529b652b0d73dc95
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-12 13:23:11 +0530

    logout button bit changes

--- src/app/Components/commonComponent/logoutButton.js
+++ src/app/Components/commonComponent/logoutButton.js
@@ -4,7 +4,6 @@ import { TbLogout2 } from "react-icons/tb"
 function LogoutButton() {
   return (
      <button onClick={logout} className='flex md:ml-4 px-2 py-2 bg-[#C94C73] text-white md:py-2 md:px-4 rounded-lg md:mt-4 items-center'><TbLogout2 className='mr-2'/>Logout</button>
-    
   )
 }
 
