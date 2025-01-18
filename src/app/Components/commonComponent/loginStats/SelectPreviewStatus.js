442943c905fe8400f127914b7c37e452b283972a
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-18 23:48:19 +0530

    option availability for recent logins 1

--- src/app/Components/commonComponent/loginStats/SelectPreviewStatus.js
+++ src/app/Components/commonComponent/loginStats/SelectPreviewStatus.js
@@ -1,1 +1,12 @@
 'use client'
+import { useState } from "react"
+
+const SelectPreviewStatus = () => {
+    const [selected, setSelected] = useState("")
+
+    return (
+        <>
+        
+        </>
+    )
+}
