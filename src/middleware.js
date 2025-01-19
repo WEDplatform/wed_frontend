ef4be03fa31ee227b45edbf8a9840891243c56c5
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-19 22:53:56 +0530

    middleware change basic line change 2

--- src/middleware.js
+++ src/middleware.js
@@ -25,7 +25,6 @@ export async function middleware(request) {
       return NextResponse.redirect(home);
     }
     }
-    
   } catch (error) {
     cookieStore.delete('refreshToken')
     console.log(error);
