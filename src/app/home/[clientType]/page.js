e54afa6033466e27dce10e220c11d216180c6355
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-28 00:28:27 +0530

    strcutctre change

--- src/app/home/[clientType]/page.js
+++ src/app/home/[clientType]/page.js
@@ -12,39 +12,40 @@ import { profileFetch } from "@/app/apiFunctions/profile"
 async function page({params,searchParams}) {
   const clientParam=await params;
   const searchprm=await searchParams
-  console.log(searchprm);
+  ///console.log(searchprm);
   let rsp=await profileFetch()
  // console.log(rsp);
   
   return (
     <>
-      <div id="mainPost" className='flex md:flex-row h-[85vh] md:h-[100vh] overflow-y-auto flex-col'>
-        <div className='w-[20%] hidden md:block border-r-2 border-gray-300 h-screen'>
+      <div id="mainPost" className='flex md:flex-row w-[100%] h-[85vh] md:h-[100vh] overflow-y-auto flex-col'>
+        {/* <div className='w-[20%] hidden md:block border-r-2 border-gray-300 h-screen'>
           <NavBar />
           <SideBarMain client={clientParam} tabParams={searchprm} />
-        </div>
+        </div> */}
         <div className="md:hidden w-[100vw] ">
             <NavBar />
             <div className="w-[100%] block md:hidden">
               <div className="w-[100%]  flex justify-center"><StorySection /></div>
             </div>
-          </div>
-        {
-          searchprm?.tab=="home" || searchprm?.tab=="search" || searchprm?.tab=="viewPost" || searchprm?.tab=="profile" ? <>
-          
-          <div className='md:w-[55%] flex flex-col items-center relative md:items-start h-[100%] w-[100vw]'>
+        </div>
+        <div className='md:w-[68%]  flex flex-col items-center relative md:items-start h-[100%] w-[100vw]'>
             <div className="w-[100%] hidden md:block">
               <div className="w-[100%]  flex justify-center"><StorySection /></div>
             </div>
             <Posts id_={"mainPost"}/>
-          </div>
+        </div>
+        {/* {
+          searchprm?.tab=="home" || searchprm?.tab=="search" || searchprm?.tab=="viewPost" || searchprm?.tab=="profile" ? <>
+          
+          
           </> : <>
           <div className="md:w-[55%] flex flex-col items-center relative md:items-start h-[100%] w-[100vw]">
 
           </div>
           </>
-        }
-        <div className="hidden md:block w-[25%] h-[100%] border-2">
+        } */}
+        <div className="hidden md:block w-[32%] h-[100%] border-2">
             <div className="w-[100%] h-[100%] flex items-center justify-center">
             {
               searchprm.tab == "search" ? <Search/> : <>
