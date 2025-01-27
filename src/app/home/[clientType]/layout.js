eee61c09bf58d4c0a35f0189481a128b5aa2f920
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-28 00:29:23 +0530

    strcutctre change 1

--- src/app/home/[clientType]/layout.js
+++ src/app/home/[clientType]/layout.js
@@ -5,9 +5,20 @@ export default async function HomePageLayout({ children, params, searchParams })
   //console.log(await searchParams);
   //console.log(await searchParams);
   const navParams = await params
+  const tbparams=await searchParams
+  //console.log(tbparams);
+  
   return <>
     
-    {children}
+    <main className="w-[100%] flex">
+    <div className='w-[20%] hidden md:block border-r-2 border-gray-300 h-screen'>
+          <NavBar />
+          <SideBarMain client={navParams}  />
+     </div>
+    <main className="w-[100%] md:w-[80%] ">
+      {children}
+    </main>
     <BottomNavBar />
+    </main>
   </>
 }
