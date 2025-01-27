54c845e4f1d1e9345720d24c0c8ad84d7d2666a9
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-28 00:33:20 +0530

    sidebar main change 1

--- src/app/apiFunctions/fetchPosts.js
+++ src/app/apiFunctions/fetchPosts.js
@@ -3,7 +3,6 @@ import { axiosInstance } from "../axios/axios";
 export const fetchPosts=async(index)=>{
     try {
         let resp=await axiosInstance.get(`/cmn/getPosts?searchIndex=${index}`)
-        console.log(resp?.data?.data);
         resp?.data?.data?.pics.map((i,p)=>i.p_type='post')
         return resp?.data?.data
     } catch (error) {
