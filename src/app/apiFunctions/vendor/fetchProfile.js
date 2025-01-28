64f68467330c909db5e44b90f0dd22d3188e2a27
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-01-29 01:16:15 +0530

    fetch profile function for vendors

---
+++ src/app/apiFunctions/vendor/fetchProfile.js
@@ -0,0 +1,12 @@
+import { axiosInstance } from "@/app/axios/axios";
+const fetchProfileData=async(vendorName)=>{
+    try {
+        const data= await axiosInstance.get(`/cmn/getVendorProfile?vendorName=${vendorName}`)
+        console.log(data?.data?.data);
+        return data?.data?.data
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+export {fetchProfileData}
