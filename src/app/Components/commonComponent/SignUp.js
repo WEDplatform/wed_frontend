89b7963280eca8f1d059a63807e57c7abf60a704
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-05 01:19:03 +0530

    profile page change 1

--- src/app/Components/commonComponent/SignUp.js
+++ src/app/Components/commonComponent/SignUp.js
@@ -5,7 +5,6 @@ import Vendorsignup from "@/app/Components/vendorComponent/signup"
 import Usersignup from "@/app/Components/userComponent/signup"
 import { vendorDetails as vnd } from '@/app/lib/vendorDetails'
 function SignUp({usertype,compIndex}) {
-  //console.log(compIndex);
   const searchParams = useSearchParams()
   const [vendorDetails,setvendorDetails]=useState(vnd)
   return (
