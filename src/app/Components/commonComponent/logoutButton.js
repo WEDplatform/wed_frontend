2cf4a7629ba604538f817bfdbb443536cfed8de8
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-05 01:18:08 +0530

    logout button bit change

--- src/app/Components/commonComponent/logoutButton.js
+++ src/app/Components/commonComponent/logoutButton.js
@@ -6,5 +6,4 @@ function LogoutButton() {
      <button onClick={logout} className='flex md:ml-4 px-2 py-2 bg-[#C94C73] text-white md:py-2 md:px-4 rounded-lg md:mt-4 items-center'><TbLogout2 className='mr-2'/>Logout</button>
   )
 }
-
 export { LogoutButton }
