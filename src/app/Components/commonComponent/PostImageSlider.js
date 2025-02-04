3f10c329e3ffeef2c07fa678ca451053ad33b027
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-05 01:18:29 +0530

    logout button bit change 1

--- src/app/Components/commonComponent/PostImageSlider.js
+++ src/app/Components/commonComponent/PostImageSlider.js
@@ -5,23 +5,23 @@ import Image from 'next/image';
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 function PostImageSlider({ imageResponse }) {
-    let imageSet=imageResponse?.imageData?.slice(0,6)
+    let imageSet = imageResponse?.imageData?.slice(0, 6)
     return (
         <Splide aria-label="" options={{
-            pagination:true
+            pagination: true
         }}
-         hasTrack={false}>
+            hasTrack={false}>
             <SplideTrack className="w-[100%] overflow-hidden h-[100%] rounded-xl">
                 {
                     imageSet?.map((item, index) =>
                         <SplideSlide key={index} className='md:w-[100%] h-[74vw] md:h-[20.5vw] rounded-lg '>
                             {
-                                index<6 && <Image priority style={{objectFit:'cover'}} fill={true} src={item?.src?.landscape} alt='pics' />
+                                index < 6 && <Image priority style={{ objectFit: 'cover' }} fill={true} src={item?.src?.landscape} alt='pics' />
                             }
                         </SplideSlide>
                     )
                 }
-                
+
             </SplideTrack>
             <div className="splide__arrows  md:flex justify-between w-[100%] px-0 absolute top-[50%] transform -translate-y-1/2">
                 <button className="splide__arrow hidden md:block splide__arrow--prev bg-white aspect-square rounded-full border-2 p-2 w-[2vw] h-[2vw]"><FaArrowLeft className="w-[100%] h-[100%] text-xl" /></button>
