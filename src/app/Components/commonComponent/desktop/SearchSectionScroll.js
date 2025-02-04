3b3836e830aa3adfc472ccb7b4a8361fb7528d8b
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-05 01:16:54 +0530

    formatted code for searchsectionscroll

--- src/app/Components/commonComponent/desktop/SearchSectionScroll.js
+++ src/app/Components/commonComponent/desktop/SearchSectionScroll.js
@@ -2,41 +2,41 @@
 import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
-function SearchSectionScroll({vals,title}) {
-  return (
-    <div className="w-[100%] mt-2">
-        <p className='text-sm text-gray-500 mt-2'>{title}</p>
-        <Splide
-        options={{
-            perPage:3,
-            pagination:false
-        }}
-        className="w-[100%] flex justify-center h-[18vh]" hasTrack={false}>
-                    <SplideTrack className="w-[92%] overflow-hidden h-[100%]  rounded-xl">
+function SearchSectionScroll({ vals, title }) {
+    return (
+        <div className="w-[100%] mt-2">
+            <p className='text-sm text-gray-500 mt-2'>{title}</p>
+            <Splide
+                options={{
+                    perPage: 3,
+                    pagination: false
+                }}
+                className="w-[100%] flex justify-center h-[18vh]" hasTrack={false}>
+                <SplideTrack className="w-[92%] overflow-hidden h-[100%]  rounded-xl">
                     {
-                vals.map((item,pos)=>
-                <SplideSlide key={pos} className="flex justify-center">
-                    <div className='w-[95%] h-[18vh] rounded-xl bg-white border-2'>
-                        <div className='w-[100%] h-[65%] bg-gray-200'></div>
-                        <div className='w-[100%] h-[35%] text-sm text-gray-500 mt-1 ml-1'>{item}</div>
+                        vals.map((item, pos) =>
+                            <SplideSlide key={pos} className="flex justify-center">
+                                <div className='w-[95%] h-[18vh] rounded-xl bg-white border-2'>
+                                    <div className='w-[100%] h-[65%] bg-gray-200'></div>
+                                    <div className='w-[100%] h-[35%] text-sm text-gray-500 mt-1 ml-1'>{item}</div>
 
-                    </div>
-                </SplideSlide>
-                )
-            }
-                    </SplideTrack>
-                    {
-                         <div className={`splide__arrows ${vals.length<=3?" invisible":" visible"}  md:flex justify-between w-[100%] px-0 absolute top-[50%] transform -translate-y-1/2`}>
+                                </div>
+                            </SplideSlide>
+                        )
+                    }
+                </SplideTrack>
+                {
+                    <div className={`splide__arrows ${vals.length <= 3 ? " invisible" : " visible"}  md:flex justify-between w-[100%] px-0 absolute top-[50%] transform -translate-y-1/2`}>
                         <button className="splide__arrow  md:block splide__arrow--prev bg-white aspect-square rounded-full border-2 p-2 w-[2vw] h-[2vw]"><FaArrowLeft className="w-[100%] h-[100%] text-xl" /></button>
                         <button className="splide__arrow  md:block splide__arrow--next bg-white aspect-square rounded-full border-2 p-2 w-[2vw] h-[2vw]"><FaArrowRight className="w-[100%] h-[100%] text-xl" /></button>
                     </div>
-                    }
-                </Splide>
-        <div >
-            
+                }
+            </Splide>
+            <div >
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export { SearchSectionScroll }
