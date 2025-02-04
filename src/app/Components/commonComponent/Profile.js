24e88064f2cf6432a5437d015fcd6a6c93e8c471
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-05 01:18:49 +0530

    profile page change

--- src/app/Components/commonComponent/Profile.js
+++ src/app/Components/commonComponent/Profile.js
@@ -11,39 +11,39 @@ function Profile() {
   return (
     <div className="w-[90%] md:overflow-y-auto h-[100vh]  md:h-[95%] flex flex-col items-center">
       <div className="w-[100%] flex justify-between items-center text-xl">
-        <BackButton/>
+        <BackButton />
         <p className="text-sm">Eventsmore events</p>
-        <FollowButton/>
+        <FollowButton />
       </div>
-        <div className="w-[10vw] h-[10vw] bg-gray-200 relative mt-3 rounded-full">
-            <input type="file" className="hidden"/>
-            <Image alt="profilePic" style={{objectFit:"cover"}} src={profile} />
-            <MdCameraswitch className="absolute top-[75%] bg-white w-[1.6vw] rounded-full border-2 border-gray-600 h-[1.6vw] p-1 right-[5%]"/>
-        </div>
-        <main className="flex flex-col items-center justify-center">
+      <div className="w-[10vw] h-[10vw] bg-gray-200 relative mt-3 rounded-full">
+        <input type="file" className="hidden" />
+        <Image alt="profilePic" style={{ objectFit: "cover" }} src={profile} />
+        <MdCameraswitch className="absolute top-[75%] bg-white w-[1.6vw] rounded-full border-2 border-gray-600 h-[1.6vw] p-1 right-[5%]" />
+      </div>
+      <main className="flex flex-col items-center justify-center">
         <p className="text-md font-semibold">Eventsmore events</p>
         <p className="text-sm font-light text-gray-500">Da Nang, Vietnam</p>
-        </main>
-        <div className="flex w-[100%] justify-between mt-3 py-2 rounded-md bg-gray-100">
-          <div className="w-[50%] text-center py-1 font-light text-gray-500">
-            <span className="font-semibold text-gray-700">200 </span>Following         
-          </div>
-          <div className="w-[50%] text-center py-1 font-light text-gray-500">
-            <span className="font-semibold text-gray-700">250 </span>Followers
-          </div>
+      </main>
+      <div className="flex w-[100%] justify-between mt-3 py-2 rounded-md bg-gray-100">
+        <div className="w-[50%] text-center py-1 font-light text-gray-500">
+          <span className="font-semibold text-gray-700">200 </span>Following
         </div>
-        <p className="flex justify-center mt-6 ">
-          {
-            vendorSocialHandle.map((item,pos)=>
+        <div className="w-[50%] text-center py-1 font-light text-gray-500">
+          <span className="font-semibold text-gray-700">250 </span>Followers
+        </div>
+      </div>
+      <p className="flex justify-center mt-6 ">
+        {
+          vendorSocialHandle.map((item, pos) =>
             <span className="text-[20px] text-gray-500 flex" key={pos}>{item.icon}
-            {
-              pos<2 && <LuDot className="text-[#9A2143]"/>
-            }
+              {
+                pos < 2 && <LuDot className="text-[#9A2143]" />
+              }
             </span>
-            )
-          }
-        </p>
-        <VendorBackButton/>
+          )
+        }
+      </p>
+      <VendorBackButton />
     </div>
   )
 }
