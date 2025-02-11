6ed8c21b821f84f230a41c3d255779f1f1fed3e6
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-11 19:02:29 +0530

    cities active file 8

--- src/app/Components/linkbutton.js
+++ src/app/Components/linkbutton.js
@@ -12,5 +12,4 @@ function Linkbutton({linkText="/authPage/user",linkTag,color="bg-[#C94C73]",clie
     }} className={`w-[80%] text-center text-black py-2 rounded-[25px] mt-4 ${color}`}><>{linkText}</></Link>
   )
 }
-
 export default Linkbutton
