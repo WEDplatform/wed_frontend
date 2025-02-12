9aef7f38dfdfca8f0f91d64a95dfcbbfc6bdf993
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-13 01:59:06 +0530

    modify for changeinputplaceholder

--- src/app/Components/ui/changeInputPlaceholder.js
+++ src/app/Components/ui/changeInputPlaceholder.js
@@ -3,7 +3,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { cn } from "@/app/lib/utils";
-
 export function PlaceholdersAndVanishInput({
   placeholders,
   onChange,
