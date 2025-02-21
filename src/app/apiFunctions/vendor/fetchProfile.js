c347015e6fdbfdfbf8fb7acbd3f0aaccfa0f106c
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-21 19:35:59 +0530

    review component making 3

--- src/app/apiFunctions/vendor/fetchProfile.js
+++ src/app/apiFunctions/vendor/fetchProfile.js
@@ -1,7 +1,12 @@
+import { getCookies } from "@/app/action";
 import { axiosInstance } from "@/app/axios/axios";
 const fetchProfileData=async(vendorName)=>{
     try {
-        const data= await axiosInstance.get(`/cmn/getVendorProfile?vendorName=${vendorName}`)
+        const data= await axiosInstance.get(`/cmn/getVendorProfile?vendorName=${vendorName}`,{
+            headers:{
+                'wedoraCredentials':await getCookies()
+            }
+        })
         console.log(data?.data?.data);
         return data?.data?.data
     } catch (error) {
