ca16e6f5b6ecd3253aff3349521895497576de69
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-27 17:11:23 +0530

    made component for user stats of likes and follow

---
+++ src/app/Components/userComponent/UserLikedFollowed.js
@@ -0,0 +1,32 @@
+'use client'
+
+import Image from "next/image"
+
+const VendorLiked=({dt,isFollowed})=>{
+    return(
+        <div className=" w-[100%] relative h-[30vh] bg-gray-200">
+            
+            <Image sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  priority style={{ objectFit: 'cover' }} fill={true} src={dt?.images[0]} alt='pics'/>
+            <h1 className="absolute bottom-0 w-[100%]  p-2 py-4 font-normal bg-gradient-to-t from-white to-transparent">
+                {dt?.name}
+            </h1>
+            <p className="absolute top-2 right-2 border-2 border-white text-white px-2 rounded-lg ">{dt?.isLikedBy?.length} Likes</p>
+            {
+                isFollowed && <p className="absolute top-2 left-2 border-2 border-white text-white px-2 rounded-lg ">Following</p>
+
+            }
+        </div>
+    )
+}
+const CoupleLiked=({dt})=>{
+    return(
+        <div className=" w-[100%] relative h-[30vh] bg-gray-200">
+            <Image sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  priority style={{ objectFit: 'cover' }} fill={true} src={dt?.couplecover[0]} alt='pics'/>
+            <h1 className="absolute bottom-0 w-[100%]  p-2 py-4 font-normal bg-gradient-to-t from-white to-transparent">
+                {dt?.coupleName}
+            </h1>
+            <p className="absolute top-2 right-2 border-2 border-white text-white px-2 rounded-lg ">{dt?.isLikedBy?.length} Likes</p>
+        </div>
+    )
+}
+export {VendorLiked,CoupleLiked}
