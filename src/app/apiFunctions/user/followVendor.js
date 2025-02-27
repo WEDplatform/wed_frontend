adc32face5326b214bbf5cc2532ada496e67c11b
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-27 17:12:27 +0530

    function to call vendor data

---
+++ src/app/apiFunctions/user/followVendor.js
@@ -0,0 +1,22 @@
+import { getCookies } from "@/app/action";
+import { axiosInstance } from "@/app/axios/axios";
+const followVendor=async(data)=>{
+    try {
+        const refreshToken=await getCookies()
+        let response;
+        if(refreshToken){
+             response=await axiosInstance.post('/user/followVendor',data,{
+                headers: {
+                    "wedoraCredentials":refreshToken
+                }
+            })
+        }
+        console.log(response?.data?.data);
+        
+        return response
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+export {followVendor}
