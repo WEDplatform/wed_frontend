7860613c576c834979c519fc6baefa18f9d87083
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-27 17:12:10 +0530

    vendor profile id update 1

---
+++ src/app/apiFunctions/user/UserFollowings.js
@@ -0,0 +1,41 @@
+'use client'
+import { CoupleLiked, VendorLiked } from "@/app/Components/userComponent/UserLikedFollowed";
+import { useState } from "react"
+const UserFollowStat=({userData})=>{
+    console.log(userData);
+    
+    const [userView,setView]=useState('likes')
+    return(
+        <>
+        <div className="flex md:w-[80%]  w-[95%] mt-3 mx-auto justify-center">
+                    <button onClick={()=>{setView("likes")}} className={`px-2 py-1 ${userView=="likes" ? "bg-gray-300":""}`}>Liked</button>
+                    <button onClick={()=>{setView("follows")}} className={`px-2 py-1 ${userView=="follows" ? "bg-gray-300":""}`}>Folowed</button>
+        </div>
+        {
+            userView=="likes" && <div className="grid mt-3 md:w-[80%] w-[90%] pb-[13vh]  mx-auto grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-2">
+                {
+                    userData[0]?.items.map((item,pos)=>
+                    <VendorLiked isFollowed={false} key={pos} dt={item}/>
+                    )
+                }
+                {
+                    userData[1]?.items.map((item,pos)=>
+                    <CoupleLiked key={pos} dt={item}/>
+                    )
+                }
+            </div>
+        }
+        {
+            userView =="follows" && <div className="grid mt-3 md:w-[80%] w-[90%] pb-[12vh]  mx-auto grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-2">
+                {
+                    userData[2]?.items.map((item,pos)=>
+                    <VendorLiked isFollowed={true} key={pos} dt={item}/>
+                    )
+                }
+                
+            </div>
+        }
+        </>
+    )
+}
+export {UserFollowStat}
