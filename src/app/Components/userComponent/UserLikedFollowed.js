db2a7453c9c87cc10c8cb14df6afe50de55c952f
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-27 17:17:11 +0530

    extra line removed 2

--- src/app/Components/userComponent/UserLikedFollowed.js
+++ src/app/Components/userComponent/UserLikedFollowed.js
@@ -3,7 +3,6 @@ import Image from "next/image"
 const VendorLiked=({dt,isFollowed})=>{
     return(
         <div className=" w-[100%] relative h-[30vh] bg-gray-200">
-            
             <Image sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  priority style={{ objectFit: 'cover' }} fill={true} src={dt?.images[0]} alt='pics'/>
             <h1 className="absolute bottom-0 w-[100%]  p-2 py-4 font-normal bg-gradient-to-t from-white to-transparent">
                 {dt?.name}
