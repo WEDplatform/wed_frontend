f01729c2ef1e668c5d86c260abbe812700907e30
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-28 22:10:10 +0530

    laoding page changes 2

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -4,8 +4,7 @@ import Link from "next/link"
 import { LoadingPage } from "../Components/commonComponent/loadingPage"
 async function page({searchParams}) {
  // console.log(await searchParams);
-  
- //await checkAuth()
+
   return (
     <div>
       Home page
