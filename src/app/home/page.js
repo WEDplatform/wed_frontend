fd56692db369f2cb4fe040a39803881d37a13d35
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-02-28 22:10:27 +0530

    home page changes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,7 +3,6 @@ import checkAuth from "./checkAuth"
 import Link from "next/link"
 import { LoadingPage } from "../Components/commonComponent/loadingPage"
 async function page({searchParams}) {
- // console.log(await searchParams);
 
   return (
     <div>
