fb82c06f37c1914e5cbf9e0f6f097a3b0e633327
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-01 18:27:47 +0530

    save and ideabuttons add

--- src/app/apiFunctions/user/UserFollowings.js
+++ src/app/apiFunctions/user/UserFollowings.js
@@ -10,6 +10,9 @@ const UserFollowStat=({userData})=>{
         <div className="flex md:w-[80%]  w-[95%] mt-3 mx-auto justify-center">
                     <button onClick={()=>{setView("likes")}} className={`px-2 py-1 ${userView=="likes" ? "bg-gray-300":""}`}>Liked</button>
                     <button onClick={()=>{setView("follows")}} className={`px-2 py-1 ${userView=="follows" ? "bg-gray-300":""}`}>Folowed</button>
+                    <button onClick={()=>{setView("saved")}} className={`px-2 py-1 ${userView=="saved" ? "bg-gray-300":""}`}>Saved</button>
+                    <button onClick={()=>{setView("ideas")}} className={`px-2 py-1 ${userView=="ideas" ? "bg-gray-300":""}`}>Ideas</button>
+
         </div>
         {
             userView=="likes" && <div className="grid mt-3 md:w-[80%] w-[90%] pb-[13vh]  mx-auto grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-2">
