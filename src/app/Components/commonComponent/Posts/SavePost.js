50ec2205d94a9c1604b2b7c30fc5a5b381baf8a2
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-03 01:14:57 +0530

    saved post us

--- src/app/Components/commonComponent/Posts/SavePost.js
+++ src/app/Components/commonComponent/Posts/SavePost.js
@@ -3,20 +3,34 @@ import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { FaRegBookmark } from "react-icons/fa6";
+import { useState } from 'react';
+import { FaBookmark } from "react-icons/fa6";
  function Save() {
+  let [isSaved,setSave]=useState(false)
   const [anchorEl, setAnchorEl] = React.useState(null);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
+
   };
   const handleClose = () => {
     setAnchorEl(null);
   };
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
+  let savePost=(e)=>{
+    if(!isSaved){
+      handleClick(e)
+    }else{
+      setSave(false)
+    }
+  }
   return (
     <div>
-      <span onClick={handleClick}><FaRegBookmark/></span>
-      <Popover
+      <span onClick={(e)=>{savePost(e)}}>{
+        isSaved ? <FaBookmark className='text-[#C94C73] text-2xl'/>:<FaRegBookmark className='text-[#C94C73] text-2xl'/>
+        }</span>
+      {
+         <Popover
         id={id}
         open={open}
         anchorEl={anchorEl}
@@ -26,9 +40,10 @@ import { FaRegBookmark } from "react-icons/fa6";
           horizontal: 'left',
         }}
       >
-        <Typography className='cursor-pointer hover:bg-gray-200' sx={{ p: 1,px:2 }}>Save as an Idea</Typography>
-        <Typography className='cursor-pointer hover:bg-gray-200' sx={{ p: 1,px:2 }}>Save as a Vendor</Typography>
-      </Popover>
+        <Typography onClick={()=>{setSave(true);handleClose()}} className='cursor-pointer hover:bg-gray-200' sx={{ p: 1,px:2 }}>Save as an Idea</Typography>
+        <Typography onClick={()=>{setSave(true);handleClose()}} className='cursor-pointer hover:bg-gray-200' sx={{ p: 1,px:2 }}>Save as a Vendor</Typography>
+      </Popover> 
+      }
     </div>
   );
 }
