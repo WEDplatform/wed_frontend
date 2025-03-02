f6c2f45b284c58594a6c63403cb03965a22fe859
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-03 02:31:40 +0530

    save post function call service 1

---
+++ src/app/apiFunctions/user/savePost.js
@@ -0,0 +1,15 @@
+const { getCookies } = require("@/app/action")
+const { axiosInstance } = require("@/app/axios/axios")
+
+const savePost=async(data)=>{
+    try {
+        await axiosInstance.post('/user/savePost',data,
+           {
+            headers: {
+                "wedoraCredentials":await getCookies()
+        }
+           })
+    } catch (error) {
+        
+    }
+}
