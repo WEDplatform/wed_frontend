d2a5eab1de13d1d08b0b02c786ea0ccbc9179e8a
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-02 22:47:47 +0530

    save post changes 2

--- src/app/Components/commonComponent/Posts/SavePost.js
+++ src/app/Components/commonComponent/Posts/SavePost.js
@@ -8,7 +8,6 @@ import { FaRegBookmark } from "react-icons/fa6";
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-
   const handleClose = () => {
     setAnchorEl(null);
   };
