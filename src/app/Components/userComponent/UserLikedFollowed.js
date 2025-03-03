6dd2d7cb40dcff338ed6145a1f72406d14b25cbd
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-03 10:58:04 +0530

    ideapost

--- src/app/Components/userComponent/UserLikedFollowed.js
+++ src/app/Components/userComponent/UserLikedFollowed.js
@@ -25,4 +25,26 @@ const CoupleLiked=({dt})=>{
         </div>
     )
 }
-export {VendorLiked,CoupleLiked}
+const VendorSaved=({dt})=>{
+    return(
+        <div className=" w-[100%] relative h-[30vh] bg-gray-200">
+            <Image sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  priority style={{ objectFit: 'cover' }} fill={true} src={dt?.images[0]} alt='pics'/>
+            <h1 className="absolute bottom-0 w-[100%]  p-2 py-4 font-normal bg-gradient-to-t from-white to-transparent">
+                {dt?.name}
+            </h1>
+            {/* <p className="absolute top-2 right-2 border-2 border-white text-white px-2 rounded-lg ">{dt?.isLikedBy?.length} Likes</p> */}
+        </div>
+    )
+}
+const VendorIdea=({dt})=>{
+    return(
+        <div className=" w-[100%] relative h-[30vh] bg-gray-200">
+            <Image sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  priority style={{ objectFit: 'cover' }} fill={true} src={dt?.images[0]} alt='pics'/>
+            <h1 className="absolute bottom-0 w-[100%]  p-2 py-4 font-normal bg-gradient-to-t from-white to-transparent">
+                {dt?.name}
+            </h1>
+            {/* <p className="absolute top-2 right-2 border-2 border-white text-white px-2 rounded-lg ">{dt?.isLikedBy?.length} Likes</p> */}
+        </div>
+    )
+}
+export {VendorLiked,CoupleLiked,VendorIdea,VendorSaved}
