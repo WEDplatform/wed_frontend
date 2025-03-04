d048ebddf587f9d582c6bd22c8421c2526c6be94
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-04 20:06:27 +0530

    notification icon add

--- src/app/Components/commonComponent/chatpen/SideWindow.js
+++ src/app/Components/commonComponent/chatpen/SideWindow.js
@@ -3,7 +3,7 @@ import { TbNotification } from "react-icons/tb";
 const SideWindow=()=>{
     return(
         <div className="md:w-[30%] w-[100%] border-2 h-[100%]">
-            <div className="px-1 py-2 flex justify-between">
+            <div className="px-4 py-3 flex justify-between">
                 <span>Chats</span>
                 <span>
                     <TbNotification/>
