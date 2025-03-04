f6292a6825d0e4660e1a121bc68f669725c87001
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-04 20:49:32 +0530

    bit update 1

--- src/app/Components/commonComponent/chatpen/notifyBar.js
+++ src/app/Components/commonComponent/chatpen/notifyBar.js
@@ -1,9 +1,16 @@
+'use client'
 import Image from "next/image"
 import profpic from "../../../../../public/profPic.png"
-const NotifyBar=()=>{
+import { useRouter } from "next/navigation"
+const NotifyBar=({user})=>{
+    const router=useRouter()
     return(
-        <div>
-            <Image src={profpic} width={40} height={40} alt="profilePicture"/>
+        <div onClick={()=>{router.push(`/chatpen/${user}?cs=1`)}} className="flex items-center px-4 py-2 hover:bg-gray-100">
+            <Image src={profpic} width={50} height={50} alt="profilePicture" className="rounded-full mr-2"/>
+            <div>
+                <h1 className="text-md font-medium">wedora events</h1>
+                <span className="text-sm text-gray-500">last updated messag</span>
+            </div>
         </div>
     )
 }
