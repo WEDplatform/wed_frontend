aad2cbb0cdcbbd31f40be32438fbf65eb356d8fa
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-04 20:04:11 +0530

    chat adds

--- src/app/Components/commonComponent/chatpen/SideWindow.js
+++ src/app/Components/commonComponent/chatpen/SideWindow.js
@@ -1,7 +1,12 @@
+import { TbNotification } from "react-icons/tb";
+
 const SideWindow=()=>{
     return(
-        <div className="md:w-[20%] w-[100%] border-2 h-[100%]">
-            sidewindow
+        <div className="md:w-[30%] w-[100%] border-2 h-[100%]">
+            <div>
+                <span>Chats</span>
+                <span></span>
+            </div>
         </div>
     )
 }
