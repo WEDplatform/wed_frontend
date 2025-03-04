5e9d9e65515decb5d0c50cb4239010c96b0610c1
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-04 19:53:38 +0530

    side window making 4

--- src/app/Components/commonComponent/chatpen/SideWindow.js
+++ src/app/Components/commonComponent/chatpen/SideWindow.js
@@ -1,6 +1,8 @@
 const SideWindow=()=>{
     return(
-        <div></div>
+        <div className="md:w-[20%] w-[100%] border-2 h-[100%]">
+            sidewindow
+        </div>
     )
 }
 export {SideWindow}
