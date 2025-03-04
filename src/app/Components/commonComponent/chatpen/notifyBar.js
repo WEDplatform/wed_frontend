111de41975f9507a25e365f2fb41dc760e02e442
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-04 20:25:50 +0530

    each user bars 4

--- src/app/Components/commonComponent/chatpen/notifyBar.js
+++ src/app/Components/commonComponent/chatpen/notifyBar.js
@@ -1,7 +1,9 @@
+import Image from "next/image"
+import profpic from "../../../../../public/profPic.png"
 const NotifyBar=()=>{
     return(
         <div>
-            
+            <Image src={profpic} width={40} height={40} alt="profilePicture"/>
         </div>
     )
 }
