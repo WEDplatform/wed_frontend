acb68046e123237f73762f1f3d9e9dd5ab972261
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-05 00:26:47 +0530

    chatsection changes 2

--- src/app/Components/commonComponent/chatpen/chatsection.js
+++ src/app/Components/commonComponent/chatpen/chatsection.js
@@ -7,8 +7,12 @@ const ChatSection=()=>{
         console.log(searchParam.get('cs'))
     },[searchParam])
     return(
-        <div className="w-[100%] h-[100vh] bg-gray-300">
-        <p className="w-[100%] text-center my-auto bg-red-200">Your personal chat arena</p>
+        <div className="w-[100%] h-[100vh] flex justify-center items-center">
+        {
+            searchParam.get('cs')=='0'?
+            <p className=" text-center text-sm ">Your personal chat arena</p>:
+            <div>main chat section</div>
+        }
         </div>
     )
 }
