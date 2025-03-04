afab09424532afa1cb2147e0fb6c9aa6ef2ea567
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-04 20:05:11 +0530

    chat adds 1

--- src/app/Components/commonComponent/chatpen/SideWindow.js
+++ src/app/Components/commonComponent/chatpen/SideWindow.js
@@ -3,9 +3,11 @@ import { TbNotification } from "react-icons/tb";
 const SideWindow=()=>{
     return(
         <div className="md:w-[30%] w-[100%] border-2 h-[100%]">
-            <div>
+            <div className="px-1 py-2 flex justify-between">
                 <span>Chats</span>
-                <span></span>
+                <span>
+                    <TbNotification/>
+                </span>
             </div>
         </div>
     )
