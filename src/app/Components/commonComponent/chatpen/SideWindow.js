52e96ad4311d576336c5f4da114ab39793718b37
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-04 20:21:12 +0530

    each user bars

--- src/app/Components/commonComponent/chatpen/SideWindow.js
+++ src/app/Components/commonComponent/chatpen/SideWindow.js
@@ -1,13 +1,14 @@
 import { FiBell } from "react-icons/fi";
 const SideWindow=()=>{
     return(
-        <div className="md:w-[30%] w-[100%] border-2 h-[100%]">
-            <div className="px-4 py-3 flex justify-between">
-                <span>Chats</span>
-                <span>
+        <div className="md:w-[30%] w-[100%] flex flex-col items-center border-2 h-[100%]">
+            <div className=" py-3 w-[90%] border-b-2 flex justify-between items-center">
+                <span className="text-xl font-semibold">Chats</span>
+                <span className="text-xl">
                     <FiBell/>
                 </span>
             </div>
+
         </div>
     )
 }
