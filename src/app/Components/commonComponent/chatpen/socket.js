c97154754d71bb1d53204a754310aa67af644066
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-05 23:57:45 +0530

    url changes 1

--- src/app/Components/commonComponent/chatpen/socket.js
+++ src/app/Components/commonComponent/chatpen/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const SERVER_URL = `${process.env.NEXT_PUBLIC_SOCKET_API}/chatpen`;
 
 export const socket = io(SERVER_URL, {
-    transports: ["websocket"],
+    transports: ["websocket"], 
     autoConnect: false,  // Prevent auto-connecting before useEffect
     withCredentials: true,
 });
