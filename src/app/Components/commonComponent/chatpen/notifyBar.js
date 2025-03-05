a7bc1fa40ad4d4dff7d1056255703b91c39b9c08
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-05 23:20:48 +0530

    notify messages

--- src/app/Components/commonComponent/chatpen/notifyBar.js
+++ src/app/Components/commonComponent/chatpen/notifyBar.js
@@ -2,13 +2,13 @@
 import Image from "next/image"
 import profpic from "../../../../../public/profPic.png"
 import { useRouter } from "next/navigation"
-const NotifyBar=({user})=>{
+const NotifyBar=({user,orgName})=>{
     const router=useRouter()
     return(
-        <div onClick={()=>{router.push(`/chatpen/${user}?cs=1`)}} className="flex cursor-pointer items-center px-4 py-2 hover:bg-[#fce3e3]">
+        <div onClick={()=>{router.push(`/chatpen/${user}?cs=1&uid=${encodeURIComponent(orgName)}`)}} className="flex cursor-pointer items-center px-4 py-2 hover:bg-[#fce3e3]">
             <Image src={profpic} width={50} height={50} alt="profilePicture" className="rounded-full mr-2"/>
             <div>
-                <h1 className="text-md font-medium">wedora events</h1>
+                <h1 className="text-md font-medium">{orgName}</h1>
                 <span className="text-sm text-gray-500">last updated messag</span>
             </div>
         </div>
