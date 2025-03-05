9cf459cf0d0e5e5118623e602b5ee73900b0a105
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-05 10:30:38 +0530

    messagebubble modify 1

--- src/app/Components/commonComponent/chatpen/chatsection.js
+++ src/app/Components/commonComponent/chatpen/chatsection.js
@@ -1,11 +1,12 @@
 'use client'
 import Image from "next/image";
 import { useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef } from "react";
 import profpic from "../../../../../public/profPic.png"
 import { MdSend } from "react-icons/md";
 import { IoReturnDownBack } from "react-icons/io5";
 import { useRouter } from "next/navigation";
+import { Chatspace } from "./chatspace";
 const ChatSection=()=>{
     const searchParam=useSearchParams();
     const [message,setMessage]=useState("");
@@ -16,6 +17,15 @@ const ChatSection=()=>{
     const modifyPayload=async(mess)=>{
         setMessage(mess)
     }
+    const sendMessage=async()=>{
+        setMessages((prevMessages) => [...prevMessages, { text: message, time: "10:01 AM", sender: "me" }]);
+
+    }
+    const [messages, setMessages] = useState([
+        { text: "Hey there!", time: "10:01 AM", sender: "user" },
+        { text: "Hello! How are you?", time: "10:02 AM", sender: "me" },
+        { text: "I'm good. What about you?", time: "10:03 AM", sender: "user" },
+      ]);
     return(
         <div className="w-[100%] h-[100vh] flex justify-center items-center">
         {
@@ -27,14 +37,12 @@ const ChatSection=()=>{
                 <Image src={profpic} width={50} height={50} alt="profilePicture" className="rounded-full mr-2"/>
                 <h1>wedora events</h1>   
                 </div>
-                <div className="w-[100%] h-[80%] px-5 overflow-auto">
-                    chats section
-                </div>
+                <Chatspace message={messages}/>
                 <div className="w-[100%] flex justify-center items-center h-[10%]">
                     <div className="w-[70%] relative flex items-center justify-center border-b-2 border-[#C94C73] ">
                         <input value={message} onChange={(e)=>{modifyPayload(e.target.value)}} autoFocus type="text" placeholder="type here..." className="w-[100%] text-md px-4 py-3 tracking-wider text-md outline-none border-none "/>
                     {
-                        message && <MdSend className=" absolute text-[#C94C73] right-2 top-3 text-[25px]"/>
+                        message && <MdSend onClick={sendMessage} className=" absolute text-[#C94C73] right-2 top-3 text-[25px]"/>
 
                     }
                     </div>
