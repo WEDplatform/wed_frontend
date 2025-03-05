295827d808814c134b057b333bfd5d2ad4ec5a98
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-05 23:23:42 +0530

    url changes

--- src/app/Components/commonComponent/chatpen/socket.js
+++ src/app/Components/commonComponent/chatpen/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = "http://localhost:8888/chatpen";
+const SERVER_URL = `${process.env.NEXT_PUBLIC_SOCKET_API}/chatpen`;
 
 export const socket = io(SERVER_URL, {
     transports: ["websocket"],
