0867af70a90b6d26a3c35b4650cd9672ba71c0e5
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-06 17:47:35 +0530

    get subs func addn

---
+++ src/app/apiFunctions/getSbscribers.js
@@ -0,0 +1,20 @@
+import { axiosInstance } from "../axios/axios";
+import { getCookies } from "../action";
+
+const getSbscribers = async (user) => {
+    const refreshToken = await getCookies();
+    try {
+        const response = await axiosInstance.get(`/${user}/getSubscribers`, {
+            headers: {
+                "wedoraCredentials": refreshToken,
+            },
+        });
+        console.log(response.data);
+        
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export  {getSbscribers}
