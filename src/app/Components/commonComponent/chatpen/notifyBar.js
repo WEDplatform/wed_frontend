87d79e26a23636e4f85e0f5dbec789e59a94a908
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-06 17:46:53 +0530

    sidebar changes 1

--- src/app/Components/commonComponent/chatpen/notifyBar.js
+++ src/app/Components/commonComponent/chatpen/notifyBar.js
@@ -2,10 +2,10 @@
 import Image from "next/image"
 import profpic from "../../../../../public/profPic.png"
 import { useRouter } from "next/navigation"
-const NotifyBar=({user,orgName})=>{
+const NotifyBar=({user,orgName,uid})=>{
     const router=useRouter()
     return(
-        <div onClick={()=>{router.push(`/chatpen/${user}?cs=1&uid=${encodeURIComponent(orgName)}`)}} className="flex cursor-pointer items-center px-4 py-2 hover:bg-[#fce3e3]">
+        <div onClick={()=>{router.push(`/chatpen/${user}?cs=1&uid=${encodeURIComponent(uid)}`)}} className="flex cursor-pointer items-center px-4 py-2 hover:bg-[#fce3e3]">
             <Image src={profpic} width={50} height={50} alt="profilePicture" className="rounded-full mr-2"/>
             <div>
                 <h1 className="text-md font-medium">{orgName}</h1>
