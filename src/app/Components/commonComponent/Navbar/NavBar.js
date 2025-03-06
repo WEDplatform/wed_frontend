414e88fc919183c4ba81ba98af2226b5c49307d8
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-06 14:39:44 +0530

    params changed in urls

--- src/app/Components/commonComponent/Navbar/NavBar.js
+++ src/app/Components/commonComponent/Navbar/NavBar.js
@@ -3,7 +3,7 @@ import SelectPage from "./SelectPage";
 import { FiBell } from "react-icons/fi";
 import { TbNotification } from "react-icons/tb";
 import Link from "next/link";
-function NavBar() {
+function NavBar({user}) {
   return (
     <div className="flex justify-between p-2 py-4 items-center md:border-0">
         <nav className="flex md:ml-2 ml-2 justify-around items-center">
@@ -11,8 +11,8 @@ function NavBar() {
         <SelectPage/>
         </nav>
         <div className="text-[20px] font-bold flex ">
-            <Link href="/chatpen/user?cs=0"><FiBell className="mr-2"/></Link>
-            <Link href="/chatpen/user?cs=0"><TbNotification className="mr-4"/></Link>
+            <Link href={`/chatpen/${user}?cs=0`}><FiBell className="mr-2"/></Link>
+            <Link href={`/chatpen/${user}?cs=0`}><TbNotification className="mr-4"/></Link>
         </div>
     </div>
   )
