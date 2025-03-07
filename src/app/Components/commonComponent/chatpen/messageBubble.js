0818dff30c853d48c459b102984803e969e97473
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-07 18:16:55 +0530

    chatspacemodification

--- src/app/Components/commonComponent/chatpen/messageBubble.js
+++ src/app/Components/commonComponent/chatpen/messageBubble.js
@@ -1,12 +1,13 @@
 import profpic from "../../../../../public/profPic.png"
 import Image from "next/image"
 
-const MessageBubble = ({ message,isSender }) => {
+const MessageBubble = ({ message,isSender ,sentAt}) => {
     return (
         <div className={`flex w-[100%] mt-1 ${isSender ? "justify-end" : "justify-start"}`}>
             <div className={`max-w-[40%] flex items-center  ${isSender ? "flex-row-reverse " : ""}`}>
                 <Image src={profpic} width={30} height={30} alt="profilePicture" className="rounded-full ml-2 mr-2"/>
                 <p className={`text-sm  px-3 py-2 rounded-lg ${isSender ? " bg-[#C94C73]  text-white" : "bg-[#f7e3e3] text-black"}`}>{message}</p>
+                <p className="text-[14px] text-gray-400 mx-2">{sentAt}</p>
             </div>
         </div>
     )
