7e1bafbe407a7231814f981c88d9eb6cc414928b
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-09 17:57:27 +0530

    savePost file change 1

--- src/app/apiFunctions/user/savePost.js
+++ src/app/apiFunctions/user/savePost.js
@@ -1,6 +1,5 @@
 const { getCookies } = require("@/app/action")
 const { axiosInstance } = require("@/app/axios/axios")
-
 const saveUserPost=async(data)=>{
     try {
         let response=await axiosInstance.post('/user/savePost',data,
