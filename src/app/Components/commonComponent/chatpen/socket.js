749d75a63170cd997c55b776f57f266fb2037f71
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-09 18:03:28 +0530

    packets in chatspace 7

--- src/app/Components/commonComponent/chatpen/socket.js
+++ src/app/Components/commonComponent/chatpen/socket.js
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 
 const SERVER_URL = `${process.env.NEXT_PUBLIC_SOCKET_API}/chatpen`;
-
 export const socket = io(SERVER_URL, {
     transports: ["websocket"], 
     autoConnect: false,  // Prevent auto -connecting before useEffect
