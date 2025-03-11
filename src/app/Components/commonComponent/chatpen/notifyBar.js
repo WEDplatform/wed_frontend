0abb7853788782f38b455c026ec27e2db3b73617
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-11 21:02:53 +0530

    passed user as props 1

--- src/app/Components/commonComponent/chatpen/notifyBar.js
+++ src/app/Components/commonComponent/chatpen/notifyBar.js
@@ -2,14 +2,14 @@
 import Image from "next/image"
 import profpic from "../../../../../public/profPic.png"
 import { useRouter } from "next/navigation"
-const NotifyBar=({user,orgName,uid,senderId,recieverId})=>{
+const NotifyBar=({user,orgName,uid,senderId,recieverId,lastMessage})=>{
     const router=useRouter()
     return(
         <div onClick={()=>{router.push(`/chatpen/${user}?cs=1&uid=${encodeURIComponent(uid)}&s=${encodeURIComponent(senderId)}&r=${encodeURIComponent(recieverId)}`)}} className="flex cursor-pointer items-center px-4 py-2 hover:bg-[#fce3e3]">
             <Image src={profpic} width={50} height={50} alt="profilePicture" className="rounded-full mr-2"/>
             <div>
                 <h1 className="text-md font-medium">{orgName}</h1>
-                <span className="text-sm text-gray-500">last updated messag</span>
+                <span className="text-sm text-gray-500">{lastMessage ||""}</span>
             </div>
         </div>
     )
