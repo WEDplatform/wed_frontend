5eac76194d7d8de4d6bf7ec219a77cc81d1639c4
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-12 23:38:46 +0530

    num for unseen mess

--- src/app/Components/commonComponent/chatpen/notifyBar.js
+++ src/app/Components/commonComponent/chatpen/notifyBar.js
@@ -2,15 +2,21 @@
 import Image from "next/image"
 import profpic from "../../../../../public/profPic.png"
 import { useRouter } from "next/navigation"
-const NotifyBar=({user,orgName,uid,senderId,recieverId,lastMessage})=>{
+const NotifyBar=({user,orgName,uid,senderId,recieverId,lastMessage,num})=>{
     const router=useRouter()
     return(
-        <div onClick={()=>{router.push(`/chatpen/${user}?cs=1&uid=${encodeURIComponent(uid)}&s=${encodeURIComponent(senderId)}&r=${encodeURIComponent(recieverId)}`)}} className="flex cursor-pointer items-center px-4 py-2 hover:bg-[#fce3e3]">
-            <Image src={profpic} width={50} height={50} alt="profilePicture" className="rounded-full mr-2"/>
+        <div onClick={()=>{router.push(`/chatpen/${user}?cs=1&uid=${encodeURIComponent(uid)}&s=${encodeURIComponent(senderId)}&r=${encodeURIComponent(recieverId)}`)}} className="flex justify-between cursor-pointer items-center px-4 py-2 hover:bg-[#fce3e3]">
+            <div className="flex"><Image src={profpic} width={50} height={50} alt="profilePicture" className="rounded-full mr-2"/>
             <div>
                 <h1 className="text-md font-medium">{orgName}</h1>
                 <span className="text-sm text-gray-500">{lastMessage ||""}</span>
-            </div>
+            </div></div>
+            {
+                num >0 && <p className="bg-[#9A2143] text-white py-1 px-2 rounded-md text-[12px]">
+               {num}
+                </p>
+            }
+           
         </div>
     )
 }
