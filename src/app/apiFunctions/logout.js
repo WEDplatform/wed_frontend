fb34e6786e005dacff13083e0b2ef98fd96e616e
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-17 22:22:03 +0530

    like post og removed 2

--- src/app/apiFunctions/logout.js
+++ src/app/apiFunctions/logout.js
@@ -1,6 +1,5 @@
 'use server'
 import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
 import { NextResponse } from 'next/server';
 export async function logout() {
    try {
