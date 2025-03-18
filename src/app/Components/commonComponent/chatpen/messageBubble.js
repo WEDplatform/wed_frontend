b0a39ba93d5a4f51ed03e113e6097f7dcbd9303a
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-18 22:23:59 +0530

    paging change 2

--- src/app/Components/commonComponent/chatpen/messageBubble.js
+++ src/app/Components/commonComponent/chatpen/messageBubble.js
@@ -1,6 +1,7 @@
 import profpic from "../../../../../public/profPic.png"
 import Image from "next/image"
 const MessageBubble = ({ message,isSender ,sentAt}) => {
+    
     return (
         <div className={`flex w-[100%] mt-1 ${isSender ? "justify-end" : "justify-start"}`}>
             <div className={`md:max-w-[40%] max-w-[90%] flex items-center  ${isSender ? "flex-row-reverse " : ""}`}>
