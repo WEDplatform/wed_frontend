db4d0626b78232a46c1903982a351e370bf226c2
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-18 15:19:48 +0530

    main chat section work 1

--- src/app/Components/commonComponent/chatpen/messageBubble.js
+++ src/app/Components/commonComponent/chatpen/messageBubble.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 const MessageBubble = ({ message,isSender ,sentAt}) => {
     return (
         <div className={`flex w-[100%] mt-1 ${isSender ? "justify-end" : "justify-start"}`}>
-            <div className={`max-w-[40%] flex items-center  ${isSender ? "flex-row-reverse " : ""}`}>
+            <div className={`md:max-w-[40%] max-w-[90%] flex items-center  ${isSender ? "flex-row-reverse " : ""}`}>
                 <Image src={profpic} width={30} height={30} alt="profilePicture" className="rounded-full ml-2 mr-2"/>
                 <p className={`text-sm  px-3 py-2 rounded-lg ${isSender ? " bg-[#C94C73]  text-white" : "bg-[#f7e3e3] text-black"}`}>{message}</p>
                 <p className="text-[14px] text-gray-400 mx-2">{sentAt}</p>
