ffc8131fa9f05c6401e7e9d88f24285a82cf7559
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-19 22:17:28 +0530

    message bubble change

--- src/app/Components/commonComponent/chatpen/messageBubble.js
+++ src/app/Components/commonComponent/chatpen/messageBubble.js
@@ -1,7 +1,6 @@
 import profpic from "../../../../../public/profPic.png"
 import Image from "next/image"
 const MessageBubble = ({ message,isSender ,sentAt}) => {
-    
     return (
         <div className={`flex w-[100%] mt-1 ${isSender ? "justify-end" : "justify-start"}`}>
             <div className={`md:max-w-[40%] max-w-[90%] flex items-center  ${isSender ? "flex-row-reverse " : ""}`}>
