e60f19e996545d57958a160bedbd5f7e6979747c
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-21 20:27:59 +0530

    home page minor change 6

--- src/app/Components/logo.js
+++ src/app/Components/logo.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 function logo() {
   return (
     <div className="text-3xl w-[100%] text-semibold text-black text-center"><span className="text-[#C94C73]">W</span>edora</div>
