2fb424711ed3a280be4342f666ff891fd3591f87
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-24 19:09:18 +0530

    profile caption add 2

--- src/app/Components/commonComponent/Posts/PostStructure.js
+++ src/app/Components/commonComponent/Posts/PostStructure.js
@@ -151,7 +151,7 @@ const VideoPost = ({videoContent,videoItem}) => {
     }
     return (
         <>
-            <div className="md:aspect-[3/4] overflow-hidden relative my-1 aspect-[3/4] row-span-3 border-2 rounded-xl md:col-span-2 w-[100%] ">
+            <div className="md:aspect-[3/4] overflow-hidden relative my-1 aspect-[3/4] row-span-3 border-2 rounded-2xl md:col-span-2 w-[100%] ">
                 <div className=" absolute bottom-0 left-0 z-10 text-white w-[100%] bg-gradient-to-t from-black to-transparent ">
                     <main className={`flex items-center justify-between gap-2 py-3 pl-2`}>
                     <VendorDetails user={videoItem?.ownerUsername} vid={videoItem?._id}/>
