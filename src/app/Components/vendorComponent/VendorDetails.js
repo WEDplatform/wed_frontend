125870f3578d0c0b99c9e37ae7150d4b29b3c53a
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-25 15:45:22 +0530

    profile condition change 2

--- src/app/Components/vendorComponent/VendorDetails.js
+++ src/app/Components/vendorComponent/VendorDetails.js
@@ -3,15 +3,15 @@ import React from 'react'
 import Image from 'next/image'
 import ICO from "@/app/favicon.ico"
 import { useRouter } from 'next/navigation'
-function VendorDetails({vid,user}) {
+function VendorDetails({vid,user,type,locationName}) {
     const router = useRouter()
     return (
         <>
-            <main onClick={() => { router.push(`/home/user?tab=profile&vid=${vid}&vendorName=${user}`) }} className="flex items-center cursor-pointer gap-2">
+            <main onClick={() => { router.push(`/home/user?tab=profile&vid=${vid}&vendorName=${user}&type=${type}`) }} className="flex items-center cursor-pointer gap-2">
                 <Image alt="pic" src={ICO} width={40} height={40} />
                 <span className="flex flex-col">
                     <span className="font-semibold">{user}</span>
-                    {/* <span className="font-normal text-[12px] text-gray-600">Dubai (UAE)</span> */}
+                    <span className="font-normal text-[12px] text-white">{locationName}</span>
                 </span>
             </main>
         </>
