17b3c5fe1177b42b6c9982d99f42060cd4655d06
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-27 22:05:23 +0530

    ftch api change

--- src/app/apiFunctions/vendor/fetchProfile.js
+++ src/app/apiFunctions/vendor/fetchProfile.js
@@ -2,7 +2,7 @@ import { getCookies } from "@/app/action";
 import { axiosInstance } from "@/app/axios/axios";
 const fetchProfileData=async({vendorName,type})=>{
     try {
-        const data= await axiosInstance.get(`/cmn/getVendorProfile?vendorName=${vendorName}`,{
+        const data= await axiosInstance.get(`/cmn/getVendorProfile?vendorName=${vendorName}&type=${type}`,{
             headers:{
                 'wedoraCredentials':await getCookies()
             }
