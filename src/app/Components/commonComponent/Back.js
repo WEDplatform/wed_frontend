dfab11479cdd659995fa28525ca366a3d2fdda22
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-29 00:17:53 +0530

    back button style changs

--- src/app/Components/commonComponent/Back.js
+++ src/app/Components/commonComponent/Back.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation"
 const Back = () => {
     const router = useRouter();
     return (
-        <button className="flex items-center py-2 bg-red-200 justify-center" onClick={()=>{router.back()}}><IoMdArrowRoundBack/>{" "}<span className="ml-2">Back</span></button>
+        <button className="flex items-center py-2 text-[14px] font-semibold text-gray-400 justify-center" onClick={()=>{router.back()}}><IoMdArrowRoundBack/>{" "}<span className="ml-2">Back</span></button>
     )
 }
 export {Back}
