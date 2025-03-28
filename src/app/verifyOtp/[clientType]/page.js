926e2e1c3f3fc96233981ae784456993ac729473
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-28 23:45:11 +0530

    verify otp page init

---
+++ src/app/verifyOtp/[clientType]/page.js
@@ -0,0 +1,11 @@
+import { Back } from "@/app/Components/commonComponent/Back";
+
+export default function page(){
+    return(
+        <div className="w-[100%] h-[100vh] flex justify-center items-center">
+        <div className="w-[25%] h-[50%] ">
+            <Back/>
+        </div>
+        </div>
+    )
+}
