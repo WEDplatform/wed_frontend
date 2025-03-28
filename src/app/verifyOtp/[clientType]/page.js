cea76a8d00bd07d3202ba9621aa0152310c95111
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-29 00:15:54 +0530

    verify otp boxes 1

--- src/app/verifyOtp/[clientType]/page.js
+++ src/app/verifyOtp/[clientType]/page.js
@@ -1,10 +1,12 @@
 import { Back } from "@/app/Components/commonComponent/Back";
+import { Otp } from "@/app/Components/commonComponent/Otp";
 
 export default function page(){
     return(
         <div className="w-[100%] h-[100vh] flex justify-center items-center">
         <div className="w-[25%] h-[50%] ">
             <Back/>
+            <Otp/>
         </div>
         </div>
     )
