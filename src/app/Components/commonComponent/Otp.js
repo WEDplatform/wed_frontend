b4a10d22a9c8bb414895f1c02188abb9c7e1cb42
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-29 00:15:41 +0530

    verify otp boxes

---
+++ src/app/Components/commonComponent/Otp.js
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react';
+import { useState } from 'react';
+import { OtpInput } from 'reactjs-otp-input';
+
+const Otp = () => {
+  const [otp, setOtp] = useState('');
+
+  const handleChange = (otp) => setOtp(otp);
+
+  return (
+    <OtpInput inputStyle={{border:'1px solid #C94C73',padding:'5px',margin:'5px',width:'50px',borderRadius:'5px',height:'50px'}} value={otp} onChange={handleChange} numInputs={6} separator={<span>-</span>} />
+  )
+};
+export {Otp}
