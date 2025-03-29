241b0534c99c62a1e50eee1ead028a2234656f98
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-29 22:56:54 +0530

    otp pages changes 2

--- src/app/Components/commonComponent/Otp.js
+++ src/app/Components/commonComponent/Otp.js
@@ -5,7 +5,6 @@ import { OtpInput } from 'reactjs-otp-input';
 const Otp = () => {
   const [otp, setOtp] = useState('');
   const handleChange = (otp) => setOtp(otp);
-
   return (
     <OtpInput shouldAutoFocus inputStyle={{border:'1px solid #C94C73',padding:'5px',margin:'5px',width:'50px',borderRadius:'5px',height:'50px'}} value={otp} onChange={handleChange} numInputs={6} separator={<span>-</span>} />
   )
