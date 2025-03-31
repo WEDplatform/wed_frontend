6719d9b0f1c801ac944d5d45718beb5c55e321aa
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-03-31 22:38:41 +0530

    log removes

--- src/app/Components/ui/Card.js
+++ src/app/Components/ui/Card.js
@@ -189,7 +189,6 @@ export const useOutsideClick = (
     };
     document.addEventListener("mousedown", listener);
     document.addEventListener("touchstart", listener);
-
     return () => {
       document.removeEventListener("mousedown", listener);
       document.removeEventListener("touchstart", listener);
