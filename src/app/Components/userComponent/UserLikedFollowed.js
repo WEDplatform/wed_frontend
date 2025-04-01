5465a1cd90da16929e4975ec2760a4f02308ac7b
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-01 21:10:32 +0530

    login changes 4

--- src/app/Components/userComponent/UserLikedFollowed.js
+++ src/app/Components/userComponent/UserLikedFollowed.js
@@ -1,7 +1,6 @@
 'use client'
 import Image from "next/image"
 const VendorLiked=({dt,isFollowed})=>{
-    
     return(
         <div className=" w-[100%] relative h-[30vh] bg-gray-200">
             <Image sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  priority style={{ objectFit: 'cover' }} fill={true} src={dt?.images?.[0] || dt?.couplecover?.[0]} alt='pics'/>
