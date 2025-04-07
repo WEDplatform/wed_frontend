926690611164b93736dc08c18d4d6ed32472e085
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-07 23:28:16 +0530

    profile page changes 1

--- src/app/profile/[clientType]/page.js
+++ src/app/profile/[clientType]/page.js
@@ -12,7 +12,6 @@ async function page({ params }) {
     const {userProfile}=prf?.data
     console.log(prf.data); 
     // console.log(prf.data);
-    
     let { clientType } = await params
     return (
         <>
