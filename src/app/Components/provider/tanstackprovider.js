dea31b4f736c4ccd00cd4855ded4038ca33fbca8
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-07 23:25:42 +0530

    tanstack query line faults 1

--- src/app/Components/provider/tanstackprovider.js
+++ src/app/Components/provider/tanstackprovider.js
@@ -2,7 +2,6 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-
 export const TanstackProvider = ({ children }) => {
   const [queryClient] = useState(() => new QueryClient());
 
