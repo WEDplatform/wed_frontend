7f5e5798359ae8f9dd500026d7347a4fe83b8c49
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-08 23:13:09 +0530

    story ccard

--- src/app/Components/commonComponent/stories/StorySection.js
+++ src/app/Components/commonComponent/stories/StorySection.js
@@ -7,7 +7,6 @@ import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 // import '@splidejs/react-splide/css';
 function StorySection() {
-
     return (
         <>
             <Splide
