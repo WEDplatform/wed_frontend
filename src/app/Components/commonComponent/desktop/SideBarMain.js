a7746d52c53d987b44698699575cf525826fc94a
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-04-10 23:17:03 +0530

    side bar secttion1

--- src/app/Components/commonComponent/desktop/SideBarMain.js
+++ src/app/Components/commonComponent/desktop/SideBarMain.js
@@ -12,7 +12,7 @@ function SideBarMain({tabParams}) {
    const router=useRouter()
    const srch=useSearchParams()
    tabParams={tab:srch.get('tab')}
-  
+
    return (
     <div className='w-[100%] h-[75%] flex flex-col justify-between mt-[15vh]'>
         <div>
