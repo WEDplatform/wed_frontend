af11f1f9d5895153d2deb5841b06495609993aa9
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-09-27 19:31:26 +0530

    changed files

--- src/app/apiFunctions/fetchPosts.js
+++ src/app/apiFunctions/fetchPosts.js
@@ -16,7 +16,7 @@ export const fetchPosts=async(index,per_page,filter=[])=>{
 }
 export const fetchReels=async(index,per_page)=>{
     try {
-        let resp=await pseudoAxios.get(`/cmn/getReels?searchIndex=${index}&per_page=${per_page}`)
+         let resp=await pseudoAxios.get(`/cmn/getReels?searchIndex=${index}&per_page=${per_page}`)
         resp?.data?.data?.reels.map((i,p)=>i.p_type='reel')
         //console.log(resp?.data?.data);
         return resp?.data?.data
