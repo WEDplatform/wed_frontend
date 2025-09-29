316e345dfecf665a6789fdbcee014b29db170a8e
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-09-29 19:47:36 +0530

    api ninja limit query removed

--- src/app/lib/apis.js
+++ src/app/lib/apis.js
@@ -1,12 +1,16 @@
 let fetchCity=async(cityName)=>{
+    console.log(cityName);
+    
     if(cityName!=""){
-        let resp=await fetch(`https://api.api-ninjas.com/v1/city?name=${cityName}&limit=30`,{
+        let resp=await fetch(`https://api.api-ninjas.com/v1/city?name=${cityName}`,{
             method: 'GET',
             headers: {
                 'X-Api-Key': '3Wlk1z/7EUfZob9p9l06Kg==014ARTIKUfZ9q2Jn',
             },
         })
         let data=await resp.json()
+        console.log(data);
+        
         return data
     }     
 }
