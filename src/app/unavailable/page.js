23243cedbedebbed8087ed8c480248bc351bbbef
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-09-29 19:46:40 +0530

    error page build

---
+++ src/app/unavailable/page.js
@@ -0,0 +1,21 @@
+'use client'
+import Image from "next/image";
+import errorPage from "../../../public/errorPage.svg"
+import Logo from "@/app/Components/logo.js"
+import useNetworkStatus from "../lib/networkStatus";
+ const metadata = {
+  title: "Erros Occured",
+  description:"Errors occured"
+}
+export default  function page() {
+    const isOnline = useNetworkStatus();
+    return (
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <div><Logo/></div>
+        {
+            !isOnline ? <p className="mt-4 mb-2">Oops! you are offline</p> : <p className="mt-4 mb-2">Sorry we faced an error</p>
+        }
+        <Image src={errorPage} width={500} height={500} alt="error" />
+      </div>
+    );
+}
