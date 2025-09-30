017baa59783e39091ef15c90bfe5dbf402cc4740
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-09-30 13:55:00 +0530

    made client store

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,19 @@
+// src/store/useAuthStore.js
 import { create } from "zustand";
+
+export const useAuthStore = create((set) => ({
+  user: null,
+  isAuthenticated: false,
+
+  setUser: (data) =>
+    set({
+      client: data,
+      isAuthenticated: true,
+    }),
+
+  clearUser: () =>
+    set({
+      user: null,
+      isAuthenticated: false,
+    }),
+}));
