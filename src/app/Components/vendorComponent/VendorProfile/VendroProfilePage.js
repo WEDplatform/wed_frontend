e8a6708cd932c430dbcb3376e7d35829c6dc490e
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-10-08 23:16:35 +0530

    VendroProfile page section1

--- src/app/Components/vendorComponent/VendorProfile/VendroProfilePage.js
+++ src/app/Components/vendorComponent/VendorProfile/VendroProfilePage.js
@@ -1,24 +1,42 @@
-
+import { FaStar } from "react-icons/fa6";
 
 const VendorProfileP1 = () => {
-    return(
+    const vendorStat = [{
+        type: "Following",
+        data: "98"
+    }, {
+        type: "Impressions",
+        data: "60"
+    }]
+    return (
         <div className="w-[100%] flex flex-col justify-evenly items-center">
-            <div className="h-[20%] w-[80%] bg-red-200">
-                Div1
+            <div className="h-[20%] w-[80%] flex ">
+                {
+                    vendorStat.map((item, pos) => {
+                        return <span key={pos} className="w-[33%] border-r-2 justify-center items-center flex flex-col">
+                            <span>{item.data}</span>
+                            <span className="text-[20px]">{item.type}</span>
+                        </span>
+                    })
+                }
+                <span className="w-[33%] border-r-2 justify-center items-center flex flex-col">
+                    <span>4.8</span>
+                    <span className="text-[20px]"><FaStar/></span>
+                </span>
             </div>
-            <button className="w-[80%] py-2 bg-[#9A2143] rounded-md font-semibold text-white">Edit profile</button>
-            <div className="h-[60%] w-[80%]">
-            Calender
+            <button className="w-[80%] py-2 bg-[#9A2143] rounded-lg font-medium text-white">Edit profile</button>
+            <div className="h-[70%] w-[80%]">
+                Calender
             </div>
         </div>
     )
 }
 const VendorProfileP2 = () => {
-    return(
+    return (
         <div className="w-[100%] bg-gray-400">
-           Main COntent2
+            Main COntent2
 
         </div>
     )
 }
-export { VendorProfileP1,VendorProfileP2 }
+export { VendorProfileP1, VendorProfileP2 }
