995d6a42deab8ba835b3b91921b40a29b7eeeffe
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-10-09 22:48:14 +0530

    changed webpage name 2

--- src/app/Components/logo.js
+++ src/app/Components/logo.js
@@ -4,5 +4,4 @@ function logo() {
     <div className="text-3xl w-[100%] text-semibold text-black text-center"><span className="text-[#C94C73]">E</span>vents<span className="text-[#C94C73]">A</span>pt</div>
   )
 }
-
 export default logo
