c66df8460d89b7e9e906b50def1a31128131cd07
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-10-09 22:47:22 +0530

    changed webpage name

--- src/app/Components/logo.js
+++ src/app/Components/logo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 function logo() {
   return (
-    <div className="text-3xl w-[100%] text-semibold text-black text-center"><span className="text-[#C94C73]">W</span>edora</div>
+    <div className="text-3xl w-[100%] text-semibold text-black text-center"><span className="text-[#C94C73]">E</span>vents<span className="text-[#C94C73]">A</span>pt</div>
   )
 }
 export default logo
