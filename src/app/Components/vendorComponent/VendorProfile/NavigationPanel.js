fcdd412247a59f433203bf5d98ad3b585b305157
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-10-11 13:35:31 +0530

    added navigators option'

---
+++ src/app/Components/vendorComponent/VendorProfile/NavigationPanel.js
@@ -0,0 +1,25 @@
+'use client'
+const NavigationPanel = () => {
+    const navigators = [{
+        icon : "",
+        info : "Profile",
+        node : 1
+    },{
+        icon : "",
+        info : "Dashboard",
+        node : 2
+    },{
+        icon : "",
+        info : "Settings",
+        node : 3
+    },{
+        icon : "",
+        info : "Logout",
+        node : 4
+    }]
+    return (
+        <div className="flex ">
+        </div>
+    )
+}
+export { NavigationPanel }
