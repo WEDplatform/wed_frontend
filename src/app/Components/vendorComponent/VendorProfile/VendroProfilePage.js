94c61a9a9cb9f5586a4337ae4cf92f0e29aa5818
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-11-27 07:50:11 +0530

    unknown changes

--- src/app/Components/vendorComponent/VendorProfile/VendroProfilePage.js
+++ src/app/Components/vendorComponent/VendorProfile/VendroProfilePage.js
@@ -15,22 +15,24 @@ const VendorProfileP1 = () => {
     return (
         <div className="w-[100%] flex flex-col justify-evenly items-center">
             <ProfilePopper/>
-            <div className="h-[20%] w-[80%] flex justify-items-center">
-                <div className="w-[10vw] h-[10vw] bg-white-200 relative rounded-full">
+            <div className="h-[20%]  w-[80%] flex justify-center">
+                <div className="w-[10vw]  h-[10vw] bg-white-200 relative rounded-full">
                         <ProfilePicture />
                 </div>
-                {
+                <div className="flex w-[50%] justify-around">
+                    {
                     vendorStat.map((item, pos) => {
-                        return <span key={pos} className="w-[33%] border-r-2 justify-center items-center flex flex-col">
+                        return <span key={pos} className=" justify-center items-center flex flex-col">
                             <span>{item.data}</span>
                             <span className="text-[20px]">{item.type}</span>
                         </span>
                     })
                 }
-                <span className="w-[33%] border-r-2 justify-center items-center flex flex-col">
+                <span className=" justify-center items-center flex flex-col">
                     <span>4.8</span>
                     <span className="text-[20px]"><FaStar/></span>
                 </span>
+                </div>
             </div>
             <NavigationPanel/>
             <div className="h-[70%] w-[80%] border-2">
