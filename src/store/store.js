5852ca7f71bc4bd0c5b9943d00b07083c15c655e
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-11-29 09:01:08 +0530

    added local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 // src/store/useAuthStore.js
 import { create } from "zustand";
-
+import { persist } from "zustand/middleware"
 export const useAuthStore = create((set) => ({
   user: null,
   isAuthenticated: false,
@@ -17,3 +17,24 @@ export const useAuthStore = create((set) => ({
       isAuthenticated: false,
     }),
 }));
+
+export const useMediaStore = create((set) => ({
+  mediaData: [],
+  setMediaData: (media) => set({ mediaData: [...mediaData, media] }),
+  recommendationTags: [],
+  setRecommendationTags: (tags) => set({ recommendationTags: [...recommendationTags, tags] })
+}))
+
+export const usePersistentStore = create(
+  persist((set) => ({
+    darkMode: false,
+    setDarkMode: () => set({ darkMode: !darkMode }),
+    mediaData: [],
+    setMediaData: (media) => set({ mediaData: [...mediaData, media] }),
+    recommendationTags: [],
+    setRecommendationTags: (tags) => set({ recommendationTags: [...recommendationTags, tags] })
+  })),
+  {
+    name: "eventsapt-store"
+  }
+)
