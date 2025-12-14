3e7584a9d2e4721115b7f1075e6f240cc46152f3
ujjwal0414 <ujjwal.gupta.phy22@itbhu.ac.in>
2025-12-14 20:06:09 +0530

    tailwind css break fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,41 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//       aspectRatio:{
+//         '4/3': '4 / 3',
+//         '16/9': '16 / 9',
+//         '9/16': '9 / 16',
+//         '3/4': '3 / 4',
+//         '1/1': '1 / 1',
+//         '2/3': '2 / 3',
+//         '3/4': '3 / 4',
+//         '1/2': '1 / 2',
+//         '1/3': '1 / 3',
+//         '2/4': '2 / 4',
+//         '3/5': '3 / 5',
+//         '1/4': '1 / 4',
+//         '1/5': '1 / 5',
+//         '1/6': '1 / 6',
+//         '2/5': '2 / 5',
+//       }
+//     },
+//   },
+//   plugins: [],
+// };
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -11,23 +43,22 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      aspectRatio:{
-        '4/3': '4 / 3',
-        '16/9': '16 / 9',
-        '9/16': '9 / 16',
-        '3/4': '3 / 4',
+      aspectRatio: {
         '1/1': '1 / 1',
-        '2/3': '2 / 3',
-        '3/4': '3 / 4',
         '1/2': '1 / 2',
         '1/3': '1 / 3',
-        '2/4': '2 / 4',
-        '3/5': '3 / 5',
         '1/4': '1 / 4',
         '1/5': '1 / 5',
         '1/6': '1 / 6',
+        '2/3': '2 / 3',
+        '2/4': '2 / 4',
         '2/5': '2 / 5',
-      }
+        '3/4': '3 / 4',
+        '3/5': '3 / 5',
+        '4/3': '4 / 3',
+        '16/9': '16 / 9',
+        '9/16': '9 / 16',
+      },
     },
   },
   plugins: [],
